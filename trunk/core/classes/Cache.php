0d085d2342686d6c40eb9f08cd6c36d634ae0544
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2010-10-09 02:44:46 +0000

    add cache and store for core

---
+++ trunk/core/classes/Cache.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * cache操作接口
+ *
+ *
+ **********************************************************
+ * 实现基于命名的cache调用接口                            
+ *      例：g_val   将存储于global变量中
+ *          g_a_m_f 将存储于global,apc,memcache,file中
+ *                  且读取顺序为依次读取
+ **********************************************************
+ */
+class Cache
+{
+        private static config = array(
+                'g' => 'global',
+                'a' => 'apc',
+                'm' => 'memcache',
+                'f' => 'file'
+        );
+
+        public static function set()
+        {
+        
+        }
+
+        public static function get()
+        {
+        
+        }
+        
+        public static function getKey($prefix,$hint)
+        {
+        
+        }
+}
---
+++ trunk/core/classes/store/Memcache.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Memcache Cache
+ *
+ *
+ */
+class Memcache
+{
+        public static function get($key)
+        {
+        
+        }
+        public static function set($key,$value,$overwrite=true)
+        {
+        
+        }
+}
