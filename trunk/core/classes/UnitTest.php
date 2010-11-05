c008b983aa51077ace092a8902f701decf504e68
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2010-11-05 07:47:44 +0000

    change unittest directory

--- trunk/core/classes/UnitTest.php
+++ trunk/core/classes/UnitTest.php
@@ -4,7 +4,7 @@ namespace Ky\Core;
 //do class autoload
 require_once __DIR__ .'/ClassLoader.php';
 
-Ky\Core\ClassLoader::init();
+ClassLoader::init();
 ClassLoader::register('ky\\core', __DIR__.'/classes/');
 
 require_once 'PHPUNIT/Framework/TestCase.php';
