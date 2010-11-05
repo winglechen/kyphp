f3ad5fb3bb6673ff6da536843e5a635ff77f3358
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2010-11-05 06:09:49 +0000

    add classloaderTestCase

---
+++ core/test/ClassLoaderTest.php
@@ -0,0 +1,27 @@
+<?php
+require_once '../classes/ClassLoader.php';
+require_once 'PHPUnit/Framework/TestCase.php';
+
+/**
+ *      ClassLoader test cases
+ *
+ */
+class ClassLoaderTest extends PHPUnit_Framework_TestCase
+{
+        private $ClassLoader;
+
+        protected function setUp()
+        {
+                parent::setUp();
+        }
+
+        protected function tearDown()
+        {
+                parent::tearDown();
+        }
+
+        public function test_should_class_loader_work_fine()
+        {
+        
+        }
+}
