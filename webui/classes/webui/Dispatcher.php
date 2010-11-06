17aa6121efc023012a37be76e9e456b5455af560
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2010-11-06 15:35:55 +0000

    dispatcher ....

--- webui/classes/webui/Dispatcher.php
+++ webui/classes/webui/Dispatcher.php
@@ -6,27 +6,38 @@
 namespace KY\Webui;
 
 use \Ky\Core\ClassLoader;
+use \Ky\Core\ConfigLoader;
 
 class Dispatcher
 {
-        public function __construct()
-        {
+    public function __construct()
+    {
+        //autoload init
+        self::initAutoLoad();
+    
+        //load global Config
         
-        }
 
-        public function init()
-        {
-        
-        }
+        //router init and parse the url
+    }
 
-        public function initAutoLoad()
-        {
-        
-        }
+    public function initAutoLoad()
+    {
+        require_once __DIR__ . '/../core/ClassLoader.php';
 
-        public static function run()
-        {
-        
-        }
+        ClassLoader::init();
+        ClassLoader::register('ky\\core', __DIR__ . '/../core/');
+        ClassLoader::register('ky\\webui',__DIR__ );
+    }
+
+    public static function run()
+    {
+        $dsp = new self();
+        $dsp->dispatch();
+    }
+    private function dispatch()
+    {
+    
+    }
 }
 
