12e0c4cac3d7760f676b518218fe3851c936b403
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2010-11-07 15:43:40 +0000

    core changes

--- core/classes/Config.php
+++ core/classes/Config.php
@@ -7,12 +7,27 @@ class Config
 
     public static function loadIni($filename,$process_section=false,$scanner_mode=false)
     {
-         return parse_ini_file($filename,$process_section,$scanner_mode);
+        //get file name
+        $name = Path::getFileName($filename);
+        $filename = realpath($filename);
+        self::$config[$name] = parse_ini_file($filename,$process_section,$scanner_mode);
     }
     
     public static function get($key)
     {
-    
+        $keys = explode('/',$key);
+        $result = null;
+        for($i=0,$cnt=count($keys); $i<$cnt; $i++){
+            if($i=0 && isset(self::$config[$keys[0]])){
+                $result = self::$config[$keys[0]]
+            }elseif(!empty($keys[$i]) && isset($result[$keys[$i]])){
+                $result = $result[$keys[$i]]
+            }else{
+                $result = null;
+                break;
+            }
+        }
+        return $result;
     }
 
     public static function set($key,$value)
--- core/classes/Path.php
+++ core/classes/Path.php
@@ -3,15 +3,31 @@ namespace Ky\Core;
 
 class Path
 {
-        private static $pathes = array();
+    private static $pathes = array();
 
-        public static register($key,$path)
-        {
-                self::$pathes[$key] = $path; 
-        }
+    public static function register($key,$path)
+    {
+        self::$pathes[$key] = $path; 
+    }
 
-        public static get($key)
-        {
-                return isset(self::$pathes[$key]) ? self::$pathes[$key] : '';
-        }
+    public static function get($key)
+    {
+        return isset(self::$pathes[$key]) ? self::$pathes[$key] : '';
+    }
+
+    public static function getFileName($path)
+    {
+        $info = pathinfo($path);
+        return basename($info['basename'],'.'.$info['extension']);
+    }
+
+    public static function getFileExt($path)
+    {
+        return pathinfo($path,PATHINFO_EXTENSION);
+    }
+
+    public static function getFileInfo($path)
+    {
+        return pathinfo($path);
+    }
 }
