19349e0dd1b8018f7c57eacd21517ba0e4a8c199
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2010-11-07 03:01:59 +0000

    Config update

--- trunk/core/classes/Config.php
+++ trunk/core/classes/Config.php
@@ -3,9 +3,20 @@ namespace Ky\Core;
 
 class Config
 {
-        public static function loadIni($filename,$process_section=false,$scanner_mode=false)
-        {
-                return parse_ini_file($filename,$process_section,$scanner_mode);
-        }
-        
+    private static $config = array();
+
+    public static function loadIni($filename,$process_section=false,$scanner_mode=false)
+    {
+         return parse_ini_file($filename,$process_section,$scanner_mode);
+    }
+    
+    public static function get($key)
+    {
+    
+    }
+
+    public static function set($key,$value)
+    {
+    
+    }
 }
