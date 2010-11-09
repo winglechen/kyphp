0d6b0ea7f6b22a7fb0256b168e02f908baf690ad
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2010-11-09 07:46:54 +0000

    add layout demoes

---
+++ trunk/webui/classes/webui/layouts/test_cat1_1_layout.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *  test cat1_1
+ */
+?>
+<?php $this->_block('right'); ?>
+    <div id="nav">
+        <?php $this->_block('nav'); $this->_endblock(); ?>
+    </div>
+    <div id="article">
+        <?php $this->_block('article'); $this->_endblock(); ?>
+    </div>
+<?php $this->_endblock(); ?>
---
+++ trunk/webui/classes/webui/layouts/test_root_tpl.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title><{title}></title>
+</head>
+<body>
+<div id="header">
+    <{header}>
+</div>
+<div id="content">
+    <{content}>
+</div>
+<div id="footer">
+    <{footer}>
+</div>
+</body>
+</html>
