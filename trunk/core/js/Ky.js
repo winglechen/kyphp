c86b48e2d240a8a99824a8b0fee786ba9fd9d756
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-01-04 02:36:02 +0000

    add Ky.js

---
+++ trunk/core/js/Ky.js
@@ -0,0 +1,18 @@
+(function(){
+    var Ky = {};
+    Ky.loader = {
+        load : function(){}
+    };
+    Ky.event  = {
+        //event handle
+        register    : function(){},
+        bind        : function(){},
+        unbind      : function(){},
+        fire        : function(){},
+        group       : function(){},
+
+    };
+    Ky.util   = {
+    
+    };
+})();
