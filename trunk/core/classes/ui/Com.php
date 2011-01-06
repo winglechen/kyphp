1dc39d46b78c72760190df5a11de87c9e64bae04
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-01-06 09:41:52 +0000

    add some actions for com

--- trunk/core/classes/ui/Com.php
+++ trunk/core/classes/ui/Com.php
@@ -4,5 +4,57 @@ use Ky\Core\Exception;
 
 class Com
 {
+    protected $id           = '';
+    protected $loadEvent    = 'alone';      // alone | suite
+    protected $loadType     = 'sync';       // async | sync
+    protected $resources    = array();
+    protected $config       = array();
+    protected $tmpUrl       = array();
 
+    /**
+     * 组件注册 onlyForSuite
+     *
+     *@return object eventHandler
+     */
+    protected function reg()
+    {
+        $this->load();
+        $this->rewriteUrl();
+        $obj = $this;
+        return array(
+            'url'       => $this->tmpUrl,
+            'callback'  => function() use($obj){
+                $obj->callback($data);
+            }
+        );
+    }
+
+    /**
+     * url rewrite onlyForSuite
+     *
+     *@return urls
+     */
+    protected function rewriteUrl()
+    {
+        $this->tmpUrl = array();
+    }
+
+    /**
+     * suite回调函数 onlyForSuite
+     *
+     *@return string  com innerhtml
+     */
+    protected function callback()
+    {
+    
+    }
+
+    /**
+     * 加载事件 所有组件都有的方法
+     *
+     */
+    protected function load()
+    {
+    
+    }
 }
