5771538ea0f3aab9cd8f39a19ab3d6df1905bae5
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-01-28 07:26:30 +0000

    add base class Controller for \ky\Webui\Controller

---
+++ trunk/webui/classes/webui/controller/Controller.php
@@ -0,0 +1,19 @@
+<?php
+namespace Ky\Webui\Controller;
+/**
+ *  controller base class
+ */
+class Controller
+{
+    public function execute()
+    {
+    
+    }
+
+    protected function loginCheck()
+    {
+    
+    }
+
+}
+ 
