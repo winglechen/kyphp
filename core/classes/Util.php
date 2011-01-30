a51100895339bbad806562e405522ea5237181ee
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-01-30 08:33:59 +0000

    add some files

--- core/classes/Util.php
+++ core/classes/Util.php
@@ -11,5 +11,22 @@ class Util
 			fastcgi_finish_request();
 		}
 		exit;
-	}
+	} 
+    private static function base64encode($str)
+    {
+        $str = base64_encode($str);
+        $str = str_replace(array('+','/','='),array('-','_',''),$str);
+        return $str;
+    }
+
+    private static function base64decode($str)
+    {
+        $str = str_replace(array('-','_'),array('+','/'),$str);
+        $mod4 = strlen($str) % 4;
+        if ($mod4) {
+            $str .= substr('====', $mod4);
+        }
+        return base64_decode($str);
+    }
+
 }
