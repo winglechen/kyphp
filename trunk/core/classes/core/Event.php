8d207e9f5874b208b21fa5f14c9d180237a93b21
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-02-11 08:51:05 +0000

    add functions for Event

--- trunk/core/classes/core/Event.php
+++ trunk/core/classes/core/Event.php
@@ -6,20 +6,81 @@ namespace Ky\Core\Core;
  */
 class Event
 {
-    private $event  = array();
-    private $group  = array();
+    private static $event  = array();
+    private static $group  = array();
 
-    public function register($evt){}
-    public function bind($evt,$func,$caller=null){}
-    public function unbind($evt,$func){}
-
-    /**
-     *
-     */
-    public function fire($evt)
+    public static function register($evtName)
     {
+        if(!isset(self::$event[$evtName])){
+            self::$event[$evtName] = array();
+        }
     }
 
-    public function group($evt,$group){}
-    public function ungroup($evt,$group){}
+    public static function unregister($evtName)
+    {
+        if(isset(self::$event[$evtName])){
+            unset(self::$event[$evtName]);
+        }
+    }
+
+    public static function bind($evtName,$action,$caller=null,$param=null)
+    {
+        self::register($evtName);
+        $evt = array(
+            'action'    => $action,
+            'caller'    => $caller,
+            'bind'      => $param
+        );
+        self::$event[$evtName][] = $evt;
+    }
+
+    public static function unbind($evtName,$action,$caller=null)
+    {
+        if(!isset(self::$event[$evtName])){
+            return false;
+        }
+        foreach(self::$event[$evtName] as $key => $evt){
+            if($evt['caller'] == $caller && $evt['action'] == $action){
+                unset(self::$event[$evtName][$key]);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static function fire($evtName,$param=null)
+    {
+        if( !isset(self::$event[$evtName]) || empty(self::$event[$evtName]) ){
+            return ;
+        }
+
+        foreach(self::$event[$evtName] as $evt){
+            $param = array(
+                'bind' => $evt['bind'],
+                'fire' => $param,
+            );
+            if(null !== $evt['caller']){
+                if(is_object($evt['caller'])){
+                    $evt['caller']->$evt['action']($param);
+                }else{
+                    $evt['caller']::$evt['action']($param);
+                }
+            }else{
+                $evt['action']($param);
+            }
+        }
+
+        //fire the group event
+    }
+
+    public static function group($evtName,$group)
+    {
+        if(!isset(self::$group[$group])){
+            self::$group[$group];
+        } 
+    }
+
+    public static function ungroup($evt,$group)
+    {
+    }
 }
