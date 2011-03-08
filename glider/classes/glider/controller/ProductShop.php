c3dfa98f7e9ddc651ebcc6042a843497d87df37d
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-03-08 06:58:15 +0000

    add some example

---
+++ glider/classes/glider/controller/ProductShop.php
@@ -0,0 +1,50 @@
+<?php
+class ProductShow
+{
+    public function access()
+    {
+    
+    }
+
+    public function cache()
+    {
+    
+    }
+
+    public function data()
+    {
+        $url1  
+    }
+}
+
+
+tableName.fieldName
+
+
+/product/get/where:productid:eq:1/data:productid:2/
+
+url->addParam('Product.id',1,'eq');
+$data = array(
+    'access' => array('ccc',''),
+    'cache' => array()
+);
+
+
+urlParameter{
+    name:
+    value:
+    relation:
+    type: where:data:param
+    group:
+}
+
+
+product(
+    id:int:11,notNull,unsigned
+);
+
+
+post 
+1, where pid | data pid
+2, {where pid | data pid}
+3, join
