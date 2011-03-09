c0ac526fc901da478a808cb07287fbe9173ac12c
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-03-09 07:02:37 +0000

    add model.js

---
+++ trunk/model/resource/model/model.js
@@ -0,0 +1,13 @@
+{
+    'product':{
+        'id':{
+            'name'          : '',
+            'nickName'      : '',
+            'alias'         : '',
+            'notNull'       : true,
+            'dataType'      : 'int',
+
+        } 
+    },
+    '__end' : null
+}
