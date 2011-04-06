12ea201750667a6d5e1bcf2fbe93e1e78b28ca03
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-04-06 09:50:34 +0000

    add some config files

---
+++ b2b/config/boot.php
@@ -0,0 +1,10 @@
+<?php
+$dir    = __DIR__;
+
+$path   = array(
+    'img'       => 'html/img',
+    'css'       => 'html/css',
+    'js'        => 'html/js',
+    'upload'    => 'html/upload',
+);
+
