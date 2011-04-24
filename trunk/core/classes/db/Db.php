aebacff52e63c0eb5e9cfe2c836bdd666b572d8d
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-04-24 15:53:58 +0000

    some change

--- trunk/core/classes/db/Db.php
+++ trunk/core/classes/db/Db.php
@@ -3,29 +3,112 @@ namespace Ky\Core\Db;
 
 class Db
 {
+    private $sql    = array();
+    private $model  = null;
+    private $joining= false;
+
+    public static $instance = null;
+    public static function getInstance()
+    {
+        if(null == self::$instance){
+            $class = __CLASS__;
+            self::$instance = new $class();
+        }
+        return self::$instance->init();
+    }
+    
+    public static function instance($table)
+    {
+        $obj = self::getInstance();
+        return $obj->table($table);
+    }
+
     public static function insert($table)
     {
-        $dbType = self::getDbType($table);
-
+        $obj = self::instance($table);
+        return $obj->action('insert');
     }
 
     public static function select($table)
     {
-    
+        $obj = self::instance($table);
+        return $obj->action('insert');
     }
 
     public static function update($table)
     {
-    
+        $obj = self::instance($table);
+        return $obj->action('update');
     }
 
     public static function delete($table)
     {
+        $obj = self::instance($table);
+        return $obj->action('delete');
+    }
+
+    private function init()
+    {
+        $this->sql      = array();
+        $this->model    = null;
+        $this->joining  = false;
+
+        return $this;
+    }
+
+    public function table($table)
+    {
+        if(!$table) return $this;
+        $this->sql['table'] = $table;
+        return $this;
+    }
+
+    public function action($action='select')
+    {
+        $this->sql['action'] = $action;
+        return $this;
+    }
+
+
+    public function where($where)
+    {
     
     }
 
-    private static function getDbType($table)
+    public function data($data)
     {
-        
+    
+    }
+
+    public function join($table,$where,$type='inner')
+    {
+    
+    }
+
+    public function orderBy($order)
+    {
+    
+    }
+
+    public function groupBy($group)
+    {
+    
+    }
+
+    /**
+     *  limit 操作
+     *
+     *@var $start   int 
+     *@var $num     int 获取数量
+     *@var $relNum  int 实际获取数量
+     */
+    public function limit($start,$num,$relNum)
+    {
+    
+    }
+
+    public function execute($filter=null,$type='row')
+    {
+        //
     }
 }
