86651e29b38e83fa5e168cc4ab55caca09d329a1
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-04-25 10:24:48 +0000

    add db ini

--- b2b/config/boot.php
+++ b2b/config/boot.php
@@ -1,10 +1,25 @@
 <?php
 $dir    = __DIR__;
 
-$path   = array(
-    'img'       => 'html/img',
-    'css'       => 'html/css',
-    'js'        => 'html/js',
-    'upload'    => 'html/upload',
+
+//path define
+define('APP_PATH',                  $dir.'/../');
+define('LIB_PATH',                  $dir.'/../lib/');
+define('CONFIG_PATH',               $dir.'/../config/');
+define('CUBEX_PATH',                $dir.'/../cubex/');
+define('GLIDER_PATH',               $dir.'/../glider/');
+
+return array(
+    'namespace'    => array(
+        'ky\\core'      => LIB_PATH,
+        'ky\\cubex'     => CUBEX_PATH,
+        'ky\\glider'    => GLIDER_PATH,
+    ),
+    'config'        => array(
+        'db'            => 'db.ini',
+    ),        
+
 );
+
+
 
