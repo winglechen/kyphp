e9c4b26fdccd1b141285fabb615dc5adbb19c9f2
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-04-25 10:24:48 +0000

    add db ini

--- trunk/b2b/www/admin/index.php
+++ trunk/b2b/www/admin/index.php
@@ -1,3 +1,7 @@
 <?php
 //入口文件
-require_once('../../cubex/page/admin/main.php');
+//require_once('../../cubex/page/admin/main.php');
+
+require(__DIR__ . '/../../lib/core/Dispatcher.php');
+
+\Ky\Core\Core\Dispatcher::run(__DIR__.'/../../config/boot.php');
