409e86cb245446b6df25e76e0bd4f10e2e371eb6
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-04-26 05:57:40 +0000

    add intor tables

--- b2b/sql/admin_member.sql
+++ b2b/sql/admin_member.sql
@@ -2,7 +2,7 @@ use b2b;
 set names utf8;
 
 create table admin_member(
-    id smallint(6) unsigned not null primary key,
+    id smallint(6) unsigned not null auto_increment primary key,
     name varchar(30) not null default '',
     password varchar(32) not null default '123456',
     key(name,password)
