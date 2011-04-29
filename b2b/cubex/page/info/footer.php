58f0bda413567240bbff0e38b56df0a2c4622d73
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-04-29 08:24:46 +0000

    pages for info update

--- b2b/cubex/page/info/footer.php
+++ b2b/cubex/page/info/footer.php
@@ -4,7 +4,7 @@
 <div class="footer_c">
 <div class="copyright_2">
 <div class="copyright_2_left">
-   <ul>
+   <!--ul>
      <li><a href="http://corp.b2b.cn/cn/product.aspx?CategoryID=23#">渠道专区</a></li>
      <li>|</li>
      <li><a href="http://corp.b2b.cn/cn/product.aspx?CategoryID=23#">联系我们</a></li>
@@ -14,10 +14,9 @@
      <li><a href="http://corp.b2b.cn/cn/product.aspx?CategoryID=23#">法律声明</a></li>
      <li>|</li>
      <li>客户服务邮箱：Service@mainone.cn</li>
-   </ul>
+   </ul-->
 
 </div>
-<br>
 <div class="copyright_2_right">
    <ul>
      <li>&#169; 2010 浩铭集团  版权所有  京ICP备06000360号</li>
