00596be463b63b7bd51cdc245c607dcdbcd499df
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-05-03 04:17:40 +0000

    add js

---
+++ trunk/b2b/www/admin/trees/js/langs/zh.js
@@ -0,0 +1,18 @@
+
+langManager.addIndexes({
+error_1:'未知错误请重试',
+error_2:'该关键词已存在',
+willReload:'页面刷新',
+deleteConfirm:'你确实要删除该项吗?',
+doOneOperationAtATime:'Only one operation can be active at any time.',
+operationInProcess:'<img src="images/ajax-loader.gif" align="absmiddle"> 操作。。。.',
+selectNode2MakeOperation:'要操作某一项请点击此项.',
+addDocMenu:'添加网址',
+addFolderMenu:'添加分类',
+editMenu:'编辑',
+deleteMenu:'删除',
+expandAll:'展开所有',
+collapseAll:'折叠所有',
+
+missionCompleted:'操作成功!'
+});
