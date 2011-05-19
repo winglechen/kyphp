51250dbce675c2d33857065d993ac89a64ad377b
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-05-19 07:39:02 +0000

    menu changes

--- trunk/b2b/cubex/page/admin/page_index.php
+++ trunk/b2b/cubex/page/admin/page_index.php
@@ -1,7 +1,9 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<div layouth="0">
 <img src="res/pages/b2b_index.png" width="100%" height="2336px" border="0" usemap="#Map" target="_blank">
 <map name="Map">
   <area shape="rect" coords="483,193,855,346" href="#" target="_blank" title="点击添加内容">
   <area shape="rect" coords="48,247,134,284" href="#" target="_self" title="点击添加内容">
   <area shape="rect" coords="50,313,141,352" href="#" target="_self" title="点击添加内容">
 </map>
+</div>
