a4ca6afffe3847aab3a75b66424dbe3359a5e2c3
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-06-07 08:58:01 +0000

    add company gateway

--- trunk/b2b/cubex/controller/Company.php
+++ trunk/b2b/cubex/controller/Company.php
@@ -16,6 +16,19 @@ class Company
 
     private static function prepare($action)
     {
+        self::getAction($action);
+        self::getCompanyNick();
+    }
+
+    private static function getCompanyNick()
+    {
+        $host = strtolower(trim($_SERVER['SERVER_NAME']));
+        $nick = substr($host,0,strpos($host,"."));
+        $_GET['nick'] = $nick;
+    }
+    
+    private static function getAction($action)
+    {
         $act = '';
         if(!isset($_GET['p'])){
             if(!$action){
@@ -28,6 +41,7 @@ class Company
 
         self::$act = $act;
     
+
     }
 
     private static function dispatch()
