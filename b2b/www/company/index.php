6bdcb3825e12215918c89940e82bb663e2907e43
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-06-07 08:47:28 +0000

    bug fixed

--- b2b/www/company/index.php
+++ b2b/www/company/index.php
@@ -5,8 +5,7 @@ require(__DIR__ . '/../../lib/core/Dispatcher.php');
 
 $config = array(
     'appname'               => 'company',
-    'default_controller'    => 'company',
+    'default_controller'    => 'Company',
     'default_action'        => 'company/index',
 );
-
 \Ky\Core\Core\Dispatcher::run(__DIR__.'/../../config/boot.php',$config);
