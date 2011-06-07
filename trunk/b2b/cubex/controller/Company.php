9bc120e325994c7db0fc8ac83aa45385b246dbc4
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-06-07 08:31:29 +0000

    add company controller

---
+++ trunk/b2b/cubex/controller/Company.php
@@ -0,0 +1,39 @@
+<?php 
+namespace Ky\Cubex\Controller;
+
+
+/**
+ *  配置信息http接口
+ */
+class Company
+{
+    private static $act = '';
+    public static function run($action)
+    {
+        self::prepare($action);
+        self::dispatch();    
+    }
+
+    private static function prepare($action)
+    {
+        $act = '';
+        if(!isset($_GET['p'])){
+            if(!$action){
+                die('wrong parameter');
+            }
+            $act = $action;
+        } else {
+            $act = $_GET['p'];
+        }
+
+        self::$act = $act;
+    
+    }
+
+    private static function dispatch()
+    {
+        
+        require(CUBEX_PATH . 'page/' . rtrim(self::$act) . '.php');
+    }
+
+}
