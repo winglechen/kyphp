936bbac96f26c72c71c52bd97fff3963be611c50
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-06-08 09:17:18 +0000

    add form class

---
+++ core/classes/core/Form.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Ky\Core\Core;
+
+class Form
+{
+    /**
+     *  
+     */
+    public static function init($namespace=null)
+    {
+        //session_start();
+        $page = isset($_POST['page']) ? $_POST['page'] : 'page';
+        if(!empty($_POST)){
+            $_POST[$page] = 1;
+            $_SESSION['form']    = $_POST;
+        }elseif(isset($_SESSION['form']) ){
+            $_POST = $_SESSION['form']; 
+            $_POST[$page] = $_GET[$page];
+        }
+
+        if($namespace){
+            return self::post($namespace);
+        }
+    }
+
+    public static function post($namespace)
+    {
+       if(!isset($_POST['namespace']) || $namespace != $_POST['namespace']){
+            $_POST = array();
+       } 
+
+       return $_POST;
+    }
+}
