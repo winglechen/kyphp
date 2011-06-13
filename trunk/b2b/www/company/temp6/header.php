c4c50726416ef3ebb7835300e98669e64d3a5da9
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-06-13 06:04:25 +0000

    add template6

---
+++ trunk/b2b/www/company/temp6/header.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=7" />
+<title>-吊装机械设备公司</title>
+<meta content="" name=keywords />
+<meta content="" name=description />
+<link href="temp6/base/templates/css/common.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="temp6/base/js/base.js"></script>
+<script type="text/javascript" src="temp6/base/js/common.js"></script>
+<script type="text/javascript" src="temp6/base/js/form.js"></script>
+<script type="text/javascript" src="temp6/base/js/blockui.js"></script>
+<!-reload-!>
+</head>
+<body style='background:transparent url(temp6/effect/source/bg/bg.png) repeat scroll 0% 0%'>
+<div id='contain' style='width:1002px;background:url(temp6/effect/source/bg/cbg.gif);margin:0px auto;padding:0px'>
+
+<div id='top' style='width:1002px;height:372px;background:none transparent scroll repeat 0% 0%'>
+
+
+<!-- 图片/FLASH -->
+
+<div id='pdv_17041' class='pdv_class'  title='' style='width:1002px;height:370px;top:0px;left:0px; z-index:2'>
+<div id='spdv_17041' class='pdv_top' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:12px;line-height:25px;font-weight:bold;color:">
+		
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="temp6/-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+
+
+<img src="temp6/diy/pics/20100723/1279870334.jpg" border="0" width="100%" />
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 网站标志 -->
+
+<div id='pdv_10806' class='pdv_class'  title='' style='width:349px;height:83px;top:33px;left:23px; z-index:5'>
+<div id='spdv_10806' class='pdv_top' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:12px;line-height:25px;font-weight:bold;color:">
+		
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="temp6/-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+
+
+<img src="temp6/advs/pics/20100524/1274670298.jpg" border="0" />
+
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 一级导航菜单 -->
+
+<div id='pdv_10994' class='pdv_class'   style='width:618px;height:42px;top:81px;left:381px; z-index:7'>
+<div id='spdv_10994' class='pdv_top' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:12px;line-height:25px;font-weight:bold;color:">
+		导航菜单
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="temp6/-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+
+<link href="temp6/menu/templates/images/mainmenu_dolphin/A.css" rel="stylesheet" type="text/css" />
+<div id="menumain">
+<div class="mainmenuiner">
+
+<a href="temp6/index.php" target="_self" class="menumain">网站首页</a>
+
+<a href="temp6/page/html/company.php" target="_self" class="menumain">公司简介</a>
+
+<a href="temp6/news/class/" target="_self" class="menumain">新闻资讯</a>
+
+<a href="temp6/product/class/" target="_self" class="menumain">产品展示</a>
+
+<a href="temp6/page/tech/equipment.php" target="_self" class="menumain">科技实力</a>
+
+<a href="temp6/page/contact/contact.php" target="_self" class="menumain">联系我们</a>
+
+</div>
+</div>
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 轮播广告(FX) -->
+
+<div id='pdv_17044' class='pdv_class'  title='' style='width:990px;height:247px;top:123px;left:6px; z-index:8'>
+<div id='spdv_17044' class='pdv_top' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:12px;line-height:25px;font-weight:bold;color:">
+		
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="temp6/-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+<div align="center">
+  <p>
+    <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" id=scriptmain name=scriptmain codebase="http://download.macromedia.com/pub/shockwave/cabs/
+flash/swflash.cab#version=6,0,29,0" width="990" height="247">
+      <param name="movie" value="advs/templates/images/lbxml.swf?bcastr_xml_url=advs/xml/lbxml.php?g=1|">
+      <param name="quality" value="high">
+      <param name=scale value=noscale>
+      <param name="LOOP" value="false">
+      <param name="menu" value="false">
+      <param name="wmode" value="transparent">
+      <embed src="temp6/advs/templates/images/lbxml.swf?bcastr_xml_url=advs/xml/lbxml.php?g=1|" width="990" height="247"  loop="false" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" salign="T" name="scriptmain" menu="false" wmode="transparent"></embed>
+    </object>
+   
+</div>
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 全站搜索表单 -->
+
+<div id='pdv_18897' class='pdv_class'  title='全站搜索' style='width:297px;height:25px;top:31px;left:670px; z-index:16'>
+<div id='spdv_18897' class='pdv_top' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:12px;line-height:25px;font-weight:bold;color:">
+		全站搜索
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="temp6/-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+<link href="temp6/search/templates/css/searchform_simple.css" rel="stylesheet" type="text/css" />
+<script language="javascript" src="temp6/search/js/searchform.js"></script>
+
+<div class="globalsearchformzone">
+<form id="globalsearchform" method="get" action="search/index.php">
+   	<div class="globalsearchform">
+    	<input name="key" type="text" id="globalsearchform_key" value="" class="input" />
+  	</div>
+	<div class="globalsearchform1">
+  		<input name="imageField" id="button" type="image" src="temp6/search/templates/images/searchr.jpg" class="imgbutton" />
+  	</div>
+</form>
+</div>
+
+</div>
+</div>
+
+</div>
+</div>
+</div>
+
