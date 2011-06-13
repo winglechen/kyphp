02cbe062bfdcfcb686bde8e2b20b3b97ea06d3f4
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-06-13 05:38:52 +0000

    add template 3

---
+++ trunk/b2b/www/company/temp3/index.php
@@ -0,0 +1,304 @@
+<?php include 'header.php'; ?>
+<script>
+var PDV_PAGEID='1'; 
+var PDV_RP=''; 
+var PDV_COLTYPE='index'; 
+var PDV_PAGENAME='index'; 
+</script>
+
+
+<div id='content' style='width:1002px;height:426px;background:#ffffff;margin:0px auto'>
+
+
+<!-- 图片/FLASH -->
+
+<div id='pdv_13907' class='pdv_class'  title='' style='width:1002px;height:12px;top:0px;left:0px; z-index:2'>
+<div id='spdv_13907' class='pdv_content' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:12px;line-height:25px;font-weight:bold;color:">
+		
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="temp3/-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+
+
+<img src="temp3/diy/pics/20100927/1285551387.jpg" border="0" width="100%" />
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- HTML编辑区 -->
+
+<div id='pdv_12539' class='pdv_class'  title='公司简介' style='width:320px;height:190px;top:35px;left:286px; z-index:7'>
+<div id='spdv_12539' class='pdv_content' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="border:0px;height:100%;padding:0;margin:0;background:url(temp3/base/border/779/images/title.jpg) 0px 0px no-repeat">
+<div style="height:44px;border:0px;padding:0;margin:0;background:url(temp3/base/border/779/images/title.jpg) 0px 0px no-repeat">
+	<div style="float:right;width:33px;height:33px;background:url(temp3/base/border/779/images/title.jpg) -967px 0px no-repeat;"><a href="temp3/page/html/company.php" style="display:inline;"><img src="temp3/base/border/779/images/more.jpg" style="border:0px;padding-top:6px;"></a></div>
+</div>
+<div style="margin:0px;padding:0px">
+<div style="PADDING-RIGHT: 2px"><img style="PADDING-LEFT: 10px; FLOAT: right" border=0 alt="" src="temp3/index/pics/20110113/201101131294883143531.jpg" /><font style="LINE-HEIGHT: 22px; FONT-FAMILY: Verdana,Arial,Helvetica,sans-serif; COLOR: #2e2e2e; FONT-SIZE: 12px">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;杭州注塑设备制造公司创建于1996年，员工858人，占地面积16万平方米，年产塑料机械2800台套，塑料制品16000吨。是中国塑料机械协会单位，中国轻工机械协会单位，塑料加工协会单位，全国轻工系统技术先进单位，高新技术企业。</font></div>
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 自选产品列表 -->
+
+<div id='pdv_10809' class='pdv_class'  title='' style='width:722px;height:144px;top:282px;left:265px; z-index:8'>
+<div id='spdv_10809' class='pdv_content' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:12px;line-height:25px;font-weight:bold;color:">
+		
+		</div>
+		<div style="float:right;margin-right:10px;display:inline">
+		<a href="temp3/product/class" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+
+<link href="temp3/product/templates/css/productlist_roll.css" type="text/css" rel="stylesheet">
+<script src="temp3/product/js/productlist_roll.js" type="text/javascript"></script>
+<div  class="productlistx">
+	<div class="rollproductlists">
+		<div class="blkproductlistx">
+			<div class="LeftButton" id="LeftArr1"></div>
+			<div class="Cont" id="ISL_Cont_11" style="overflow:hidden;">
+
+					<div class="box">
+						<div class="fang" style="width:150px;height:109px">
+							<div class="picFit" style="width:150px;height:109px">
+							<a href="temp3/product/html/?82.html" target="_blank"><img height="109px" src="temp3/product/pics/20100114/1263441984.jpg" width="150px" border="0"></a>
+							</div>
+						</div>
+						<a href="temp3/" target="_blank" class="prodtitle">全自动高速真空成型机</a>
+					</div>
+
+					<div class="box">
+						<div class="fang" style="width:150px;height:109px">
+							<div class="picFit" style="width:150px;height:109px">
+							<a href="temp3/product/html/?81.html" target="_blank"><img height="109px" src="temp3/product/pics/20100114/1263441957.jpg" width="150px" border="0"></a>
+							</div>
+						</div>
+						<a href="temp3/" target="_blank" class="prodtitle">自动高速真空成型机</a>
+					</div>
+
+					<div class="box">
+						<div class="fang" style="width:150px;height:109px">
+							<div class="picFit" style="width:150px;height:109px">
+							<a href="temp3/product/html/?80.html" target="_blank"><img height="109px" src="temp3/product/pics/20100114/1263442011.jpg" width="150px" border="0"></a>
+							</div>
+						</div>
+						<a href="temp3/" target="_blank" class="prodtitle">EPE发泡片材回收造粒机</a>
+					</div>
+
+					<div class="box">
+						<div class="fang" style="width:150px;height:109px">
+							<div class="picFit" style="width:150px;height:109px">
+							<a href="temp3/product/html/?79.html" target="_blank"><img height="109px" src="temp3/product/pics/20100114/1263441874.jpg" width="150px" border="0"></a>
+							</div>
+						</div>
+						<a href="temp3/" target="_blank" class="prodtitle">HDX1100容压注塑机</a>
+					</div>
+
+					<div class="box">
+						<div class="fang" style="width:150px;height:109px">
+							<div class="picFit" style="width:150px;height:109px">
+							<a href="temp3/product/html/?78.html" target="_blank"><img height="109px" src="temp3/product/pics/20100114/1263441849.jpg" width="150px" border="0"></a>
+							</div>
+						</div>
+						<a href="temp3/" target="_blank" class="prodtitle">HDX78通用注塑机</a>
+					</div>
+
+					<div class="box">
+						<div class="fang" style="width:150px;height:109px">
+							<div class="picFit" style="width:150px;height:109px">
+							<a href="temp3/product/html/?77.html" target="_blank"><img height="109px" src="temp3/product/pics/20100114/1263441779.jpg" width="150px" border="0"></a>
+							</div>
+						</div>
+						<a href="temp3/" target="_blank" class="prodtitle">DX78通用注塑机</a>
+					</div>
+
+					<div class="box">
+						<div class="fang" style="width:150px;height:109px">
+							<div class="picFit" style="width:150px;height:109px">
+							<a href="temp3/product/html/?75.html" target="_blank"><img height="109px" src="temp3/product/pics/20100114/1263439292.jpg" width="150px" border="0"></a>
+							</div>
+						</div>
+						<a href="temp3/" target="_blank" class="prodtitle">HD120注塑机</a>
+					</div>
+
+					<div class="box">
+						<div class="fang" style="width:150px;height:109px">
+							<div class="picFit" style="width:150px;height:109px">
+							<a href="temp3/product/html/?74.html" target="_blank"><img height="109px" src="temp3/product/pics/20100114/1263441922.jpg" width="150px" border="0"></a>
+							</div>
+						</div>
+						<a href="temp3/" target="_blank" class="prodtitle">连杆式注塑机</a>
+					</div>
+
+			</div>
+			<div class="RightButton" id="RightArr1"></div>
+<script language="javascript" type="text/javascript">
+<!--//--><![CDATA[//><!--
+var scrollPic_03 = new ScrollPic();
+scrollPic_03.scrollContId   = "ISL_Cont_11"; //内容容器ID
+scrollPic_03.arrLeftId      = "LeftArr1";//左箭头ID
+scrollPic_03.arrRightId     = "RightArr1"; //右箭头ID
+
+scrollPic_03.frameWidth     = 688;//显示框宽度
+scrollPic_03.pageWidth      = 172; //翻页宽度
+
+scrollPic_03.speed          = 10; //移动速度(单位毫秒，越小越快)
+scrollPic_03.space          = 10; //每次移动像素(单位px，越大越快)
+scrollPic_03.autoPlay       = true; //自动播放
+scrollPic_03.autoPlayTime   = 3; //自动播放间隔时间(秒)
+
+scrollPic_03.initialize(); //初始化
+							
+		//--><!]]>
+</script>
+		</div>
+	</div>
+</div>
+<script>
+$(function() {
+$().picFit("auto");
+});
+</script>
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 产品分类（列表） -->
+
+<div id='pdv_13908' class='pdv_class'  title='' style='width:253px;height:348px;top:12px;left:0px; z-index:10'>
+<div id='spdv_13908' class='pdv_content' style='overflow:visible;width:100%;'>
+<div class="pdv_border" style="border:0px;height:100%;padding:0;margin:0;">
+<div style="height:100%;background:url(temp3/base/border/776/images/rbg.jpg) right repeat-y">
+<div style="height:44px;border:0px;padding:0;margin:0;background:url(temp3/base/border/776/images/title.jpg) 0px 0px no-repeat">
+</div>
+<div style="margin:0px;padding:0px">
+
+<link href="temp3/product/templates/css/productclass_dolphin.css" rel="stylesheet" type="text/css" />
+<div class="productclass_dolphin">
+
+
+<a href="temp3/product/class/?115.html" target="_self" class="productclass_dolphin">塑料发泡机械</a>
+
+
+<a href="temp3/product/class/?116.html" target="_self" class="productclass_dolphin">塑料包装材料机械</a>
+
+
+<a href="temp3/product/class/?74.html" target="_self" class="productclass_dolphin">木塑型、板、片材机械</a>
+
+
+<a href="temp3/product/class/?119.html" target="_self" class="productclass_dolphin">塑料网材、土工材料机械</a>
+
+
+<a href="temp3/product/class/?120.html" target="_self" class="productclass_dolphin">塑料管材机械</a>
+
+
+<a href="temp3/product/class/?124.html" target="_self" class="productclass_dolphin">塑料中空成型机</a>
+
+
+<a href="temp3/product/class/?125.html" target="_self" class="productclass_dolphin">塑料注射成型机</a>
+
+
+<a href="temp3/product/class/?126.html" target="_self" class="productclass_dolphin">塑料挤出机</a>
+ 
+</div>
+
+</div>
+</div>
+</div>
+<div style="margin-top:-9px;height:9px;line-height:9px;background:url(temp3/base/border/776/images/title.jpg) 0px -341px no-repeat">&nbsp;</div>
+<div style="float:right;margin-top:-9px;width:10px;height:9px;line-height:9px;background:url(temp3/base/border/776/images/title.jpg) -243px -341px no-repeat">&nbsp;</div>
+
+</div>
+</div>
+
+<!-- 图片/FLASH -->
+
+<div id='pdv_13909' class='pdv_class'  title='' style='width:226px;height:49px;top:370px;left:12px; z-index:11'>
+<div id='spdv_13909' class='pdv_content' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:12px;line-height:25px;font-weight:bold;color:">
+		
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="temp3/-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+
+
+<a href="temp3/page/html/sales.php"><img src="temp3/diy/pics/20100927/1285552645.jpg" border="0" width="100%" /></a>
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 文章列表 -->
+
+<div id='pdv_14393' class='pdv_class'  title='' style='width:322px;height:200px;top:34px;left:644px; z-index:15'>
+<div id='spdv_14393' class='pdv_content' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="border:0px;height:100%;padding:0;margin:0;background:url(temp3/base/border/778/images/title.jpg) 0px 0px no-repeat">
+<div style="height:36px;border:0px;padding:0;margin:0;background:url(temp3/base/border/778/images/title.jpg) 0px 0px no-repeat">
+	<div style="float:right;width:33px;height:36px;background:url(temp3/base/border/778/images/title.jpg) -967px 0px no-repeat;"><a href="temp3/news/class/?86.html" style="display:inline;"><img src="temp3/base/border/778/images/more.jpg" style="border:0px;padding-top:6px;"></a></div>
+</div>
+<div style="margin:0px;padding:0px">
+
+<link href="temp3/news/templates/css/newslist_time.css" rel="stylesheet" type="text/css" />
+<ul class="newslist_time">
+
+<li class="newslist_time"><div class="time">2009/12/29</div><a href="temp3/news/html/?346.html" target="_self" class="newslist_time"   >国际质量管理和质量保证体系国际质量体</a></li>
+
+<li class="newslist_time"><div class="time">2009/12/15</div><a href="temp3/news/html/?339.html" target="_self" class="newslist_time"   >公司全面国际质量管理和质量保证体系</a></li>
+
+<li class="newslist_time"><div class="time">2009/12/15</div><a href="temp3/news/html/?338.html" target="_self" class="newslist_time"   >管理和质量保证体系国际质量管理和质量</a></li>
+
+<li class="newslist_time"><div class="time">2009/12/15</div><a href="temp3/news/html/?336.html" target="_self" class="newslist_time"   >公司全面实施国际质量管理和质量保证体</a></li>
+
+<li class="newslist_time"><div class="time">2009/12/15</div><a href="temp3/news/html/?335.html" target="_self" class="newslist_time"   >国际质量管理和质量保证体系国际质量管</a></li>
+
+</ul>
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 空白边框 -->
+
+<div id='pdv_14394' class='pdv_class'  title='' style='width:682px;height:40px;top:239px;left:286px; z-index:16'>
+<div id='spdv_14394' class='pdv_content' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="border:0px;height:100%;padding:0;margin:0;background:url(temp3/base/border/777/images/title.jpg) 0px 0px no-repeat">
+<div style="height:44px;border:0px;padding:0;margin:0;background:url(temp3/base/border/777/images/title.jpg) 0px 0px no-repeat">
+	<div style="float:right;width:33px;height:33px;background:url(temp3/base/border/777/images/title.jpg) -967px 0px no-repeat;"><a href="temp3/product/class/" style="display:inline;"><img src="temp3/base/border/777/images/more.jpg" style="border:0px;padding-top:6px;"></a></div>
+</div>
+<div style="margin:0px;padding:0px">
+&nbsp;
+</div>
+</div>
+
+</div>
+</div>
+</div>
+
+<?php include "footer.php"; ?>
---
+++ trunk/b2b/www/company/temp3/news.php
@@ -0,0 +1,185 @@
+<?php include "header.php"; ?>
+<script>
+var PDV_PAGEID='2'; 
+var PDV_RP='temp3/'; 
+var PDV_COLTYPE='news'; 
+var PDV_PAGENAME='query'; 
+</script>
+
+<div id='content' style='width:1002px;height:591px;background:#ffffff;margin:0px auto'>
+
+
+<!-- 图片/FLASH -->
+
+<div id='pdv_14582' class='pdv_class'  title='' style='width:1002px;height:12px;top:0px;left:0px; z-index:2'>
+<div id='spdv_14582' class='pdv_content' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:12px;line-height:25px;font-weight:bold;color:">
+		
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+
+
+<img src="temp3/diy/pics/20100927/1285551387.jpg" border="0" width="100%" />
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 当前位置提示条 -->
+
+<div id='pdv_14593' class='pdv_class'  title='当前位置' style='width:756px;height:44px;top:12px;left:246px; z-index:11'>
+<div id='spdv_14593' class='pdv_content' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="border:0px;background:url(temp3/base/border/788/images/title.jpg) no-repeat;padding:0px;margin:0px;height:100%;">
+	<div style="float:right;height:100%;width:18px;background:url(temp3/base/border/788/images/title.jpg) -982px 0px no-repeat;"></div>
+ 
+<link href="temp3/news/templates/css/nav.css" rel="stylesheet" type="text/css" />
+<div id="nav">
+您现在的位置：<a href="temp3/">注塑设备制造公司</a> 
+ 
+&gt; <a href="temp3/news/" >新闻中心</a> 
+
+</div>
+
+</div>
+
+</div>
+</div>
+
+<!-- 文章翻页检索 -->
+
+<div id='pdv_14594' class='pdv_class'  title='文章检索' style='width:686px;height:500px;top:89px;left:282px; z-index:12'>
+<div id='spdv_14594' class='pdv_content' style='overflow:visible;width:100%;'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:12px;line-height:25px;font-weight:bold;color:">
+		文章检索
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+
+<link href="temp3/news/templates/css/newsquery.css" rel="stylesheet" type="text/css" />
+<div id="newsquery">
+<ul id="queryul">
+ 
+<li class="title">
+	<div class="title"><a href="temp3/news/html/?384.html" target="_self"   >飞机零部件制造公司</a></div>
+	<div class="time">2010-09-27</div>
+
+</li>
+ 
+<li class="title">
+	<div class="title"><a href="temp3/news/html/?383.html" target="_self"   >西安飞机零部件制造公司</a></div>
+	<div class="time">2010-09-27</div>
+
+</li>
+ 
+<li class="title">
+	<div class="title"><a href="temp3/news/html/?382.html" target="_self"   >上海电子股份有限公司</a></div>
+	<div class="time">2010-09-27</div>
+
+</li>
+ 
+<li class="title">
+	<div class="title"><a href="temp3/news/html/?381.html" target="_self"   >精密电子研究所</a></div>
+	<div class="time">2010-09-27</div>
+
+</li>
+ 
+<li class="title">
+	<div class="title"><a href="temp3/news/html/?380.html" target="_self"   >光纤（杭州）有限公司</a></div>
+	<div class="time">2010-09-27</div>
+
+</li>
+ 
+<li class="title">
+	<div class="title"><a href="temp3/news/html/?379.html" target="_self"   >玻璃有限公司</a></div>
+	<div class="time">2010-09-27</div>
+
+</li>
+ 
+<li class="title">
+	<div class="title"><a href="temp3/news/html/?378.html" target="_self"   >某重型卡车公司</a></div>
+	<div class="time">2010-09-27</div>
+
+</li>
+ 
+<li class="title">
+	<div class="title"><a href="temp3/news/html/?377.html" target="_self"   >涨激发勘探业石油钻探设备出口激增</a></div>
+	<div class="time">2010-02-24</div>
+
+</li>
+ 
+<li class="title">
+	<div class="title"><a href="temp3/news/html/?376.html" target="_self"   >油价上涨激发勘探业石油钻探设备出口激增 </a></div>
+	<div class="time">2010-02-24</div>
+
+</li>
+ 
+<li class="title">
+	<div class="title"><a href="temp3/news/html/?366.html" target="_self"   >涨激发勘探业石油钻探设备出口激增 </a></div>
+	<div class="time">2010-01-06</div>
+
+</li>
+
+</ul>
+</div>
+
+<div id="showpages">
+	  <div id="pagesinfo">共19条 每页10条 页次：1/2</div>
+	  <div id="pages"><ul><li class="pbutton"><a href=/1295932157/news/class/index.php?page=1&amp;catid=0&amp;myord=uptime&amp;myshownums=&amp;showtj=&amp;showdate=&amp;author=&amp;key=>首页</a></li><li class="pbutton">上一页</li><li class="pagesnow">1</li><li><a href=/1295932157/news/class/index.php?page=2&amp;catid=0&amp;myord=uptime&amp;myshownums=&amp;showtj=&amp;showdate=&amp;author=&amp;key=>2</a></li><li class="pbutton"><a  href=/1295932157/news/class/index.php?page=2&amp;catid=0&amp;myord=uptime&amp;myshownums=&amp;showtj=&amp;showdate=&amp;author=&amp;key= >下一页</a></li><li class="opt"><select onChange="window.location=this.options[this.selectedIndex].value"><option value="/1295932157/news/class/index.php?page=1&amp;catid=0&amp;myord=uptime&amp;myshownums=&amp;showtj=&amp;showdate=&amp;author=&amp;key=" selected>第1页</option><option value="/1295932157/news/class/index.php?page=2&amp;catid=0&amp;myord=uptime&amp;myshownums=&amp;showtj=&amp;showdate=&amp;author=&amp;key=">第2页</option></select></li><li class="pbutton"><a href=/1295932157/news/class/index.php?page=2&amp;catid=0&amp;myord=uptime&amp;myshownums=&amp;showtj=&amp;showdate=&amp;author=&amp;key=>尾页</a></li></ul></div>
+</div>
+
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 文章一级分类 -->
+
+<div id='pdv_14595' class='pdv_class'  title='' style='width:253px;height:200px;top:12px;left:0px; z-index:13'>
+<div id='spdv_14595' class='pdv_content' style='overflow:visible;width:100%;'>
+<div class="pdv_border" style="border:0px;height:100%;padding:0;margin:0;">
+<div style="height:100%;background:url(temp3/base/border/781/images/rbg.jpg) right repeat-y">
+<div style="height:44px;border:0px;padding:0;margin:0;background:url(temp3/base/border/781/images/title.jpg) 0px 0px no-repeat">
+</div>
+<div style="margin:0px;padding:0px">
+
+<link href="temp3/news/templates/css/newsclass_dolphin.css" rel="stylesheet" type="text/css" />
+<div class="newsclass_dolphin">
+
+
+<a href="temp3/news/class/?86.html" target="_self" class="newsclass_dolphin">公司新闻</a>
+
+
+<a href="temp3/news/class/?95.html" target="_self" class="newsclass_dolphin">行业动态</a>
+
+
+<a href="temp3/news/class/?97.html" target="_self" class="newsclass_dolphin">典型客户</a>
+ 
+</div>
+
+</div>
+</div>
+</div>
+<div style="margin-top:-9px;height:9px;line-height:9px;background:url(temp3/base/border/781/images/title.jpg) 0px -341px no-repeat">&nbsp;</div>
+<div style="float:right;margin-top:-9px;width:10px;height:9px;line-height:9px;background:url(temp3/base/border/781/images/title.jpg) -243px -341px no-repeat">&nbsp;</div>
+
+</div>
+</div>
+</div>
+
+
+<?php include "footer.php"; ?>
