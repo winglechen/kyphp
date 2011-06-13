4ec9e764908c3e71ec931541d71258484f0eb159
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-06-13 11:08:58 +0000

    add url link

--- b2b/cubex/page/www/member/website_nav_add_done.php
+++ b2b/cubex/page/www/member/website_nav_add_done.php
@@ -7,8 +7,28 @@ use Ky\Core\Uid\Uid;
 
 if(!empty($_POST)){    
     if(0 == $_POST['id']){
-        
-        Mnav::add($_POST);
+        if(0 == $_POST['urltype']){
+            $data = array(
+            'name'     => $_POST['name'],
+            'url'      => $_POST['url'],
+            );
+        }elseif(1 == $_POST['urltype']) {
+            $data = array(
+                'name'     => $_POST['name'],
+                'url'      => 'index.php?p=product&cid='.$_POST['pid'],
+            );
+        }elseif(2 == $_POST['urltype']) {
+            $data = array(
+                'name'     => $_POST['name'],
+                'url'      => 'index.php?p=news&nid='.$_POST['pid'],
+            );
+        }elseif(3 == $_POST['urltype']) {
+            $data = array(
+                'name'     => $_POST['name'],
+                'url'      => 'index.php?p=news&pid='.$_POST['pid'],
+            );
+        }
+        Mnav::add($data);
     }else{
         $data = array(
             'id'    => $_POST['id'],
