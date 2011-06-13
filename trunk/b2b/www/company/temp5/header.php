d5f3731610085d4dd5581b18a4ac962e41a7e86a
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-06-13 05:57:11 +0000

    add template 5

---
+++ trunk/b2b/www/company/temp5/header.php
@@ -0,0 +1,313 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=7" />
+<title>-机械零部件制造公司</title>
+<meta content="" name=keywords />
+<meta content="" name=description />
+<link href="temp5/base/templates/css/common.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="temp5/base/js/base.js"></script>
+<script type="text/javascript" src="temp5/base/js/common.js"></script>
+<script type="text/javascript" src="temp5/base/js/form.js"></script>
+<script type="text/javascript" src="temp5/base/js/blockui.js"></script>
+<!-reload-!>
+</head>
+<body style='background:transparent'>
+<div id='contain' style='width:1002px;background:none transparent scroll repeat 0% 0%;margin:0px auto;padding:0px'>
+
+<div id='top' style='width:1002px;height:356px;background:none transparent scroll repeat 0% 0%'>
+
+
+<!-- 图片/FLASH -->
+
+<div id='pdv_22934' class='pdv_class'  title='' style='width:1002px;height:219px;top:137px;left:0px; z-index:1'>
+<div id='spdv_22934' class='pdv_top' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:12px;line-height:25px;font-weight:bold;color:">
+		
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="temp5/http://" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+
+
+<img src="temp5/diy/pics/20101111/1289452831.jpg" border="0" width="100%" />
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 自定义透明FLASH -->
+
+<div id='pdv_22024' class='pdv_class'  title='' style='width:973px;height:197px;top:150px;left:14px; z-index:2'>
+<div id='spdv_22024' class='pdv_top' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:12px;line-height:25px;font-weight:bold;color:">
+		
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="temp5/-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+
+
+<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" width="973" height="197">
+ <param name="movie" value="diy/pics/20101028/1288247774.swf" />
+ <param name="quality" value="high" />
+ <param name="wmode" value="transparent" />
+ <embed src="temp5/diy/pics/20101028/1288247774.swf" wmode="transparent" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="100%" height="100%"></embed>
+</object>
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 图片/FLASH -->
+
+<div id='pdv_19019' class='pdv_class'  title='' style='width:1002px;height:134px;top:0px;left:0px; z-index:4'>
+<div id='spdv_19019' class='pdv_top' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:12px;line-height:25px;font-weight:bold;color:">
+		
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="temp5/http://" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+
+
+<img src="temp5/diy/pics/20100511/1273557703.jpg" border="0" width="100%" />
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 网站标志 -->
+
+<div id='pdv_19015' class='pdv_class'  title='' style='width:365px;height:64px;top:18px;left:32px; z-index:6'>
+<div id='spdv_19015' class='pdv_top' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:12px;line-height:25px;font-weight:bold;color:">
+		
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="temp5/-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+
+
+<img src="temp5/advs/pics/20100511/1273557867.jpg" border="0" />
+
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 设为首页加入收藏 -->
+
+<div id='pdv_22477' class='pdv_class'   style='width:235px;height:24px;top:5px;left:718px; z-index:10'>
+<div id='spdv_22477' class='pdv_top' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:12px;line-height:25px;font-weight:bold;color:">
+		
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="temp5/-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+
+<div style="font:12px/14px simsun">
+	<a href="temp5/http://demo.2799.cn/1294375486/" style="color:#8b8b8b;font:12px/14px simsun;" onClick="this.style.behavior='url(temp5/#default#homepage)';this.setHomePage('http://demo.2799.cn/1294375486/'); return(false);"><img src="temp5/effect/templates/images/home.jpg" border="0" style="margin:0px 5px 0px 0px;" />设为首页</a>  
+	<a href="temp5/http://demo.2799.cn/1294375486/" style="color:#8b8b8b;font:12px/14px simsun;padding-left:6px;" onClick="window.external.addFavorite('http://demo.2799.cn/1294375486/','机械零部件制造公司'); return(false);"><img src="temp5/effect/templates/images/bookmark.jpg" border="0" style="margin:0px 5px 0px 0px;" />加入收藏</a>
+	  <a href="temp5/mailto:boss@mail.com" style="color:#8b8b8b;font:12px/14px simsun;padding-left:8px;"><img src="temp5/effect/templates/images/mail.jpg" border="0" style="margin:0px 5px 0px 0px;" />联系我们</a>
+</div>
+
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 二级下拉菜单 -->
+
+<div id='pdv_23321' class='pdv_class'   style='width:812px;height:29px;top:95px;left:190px; z-index:12'>
+<div id='spdv_23321' class='pdv_top' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:12px;line-height:25px;font-weight:bold;color:">
+		导航菜单
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="temp5/-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+
+<link href="temp5/menu/templates/images/dropdownmenu_dolphin/A.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="temp5/menu/js/dropdownmenu_dolphin.js"></script>
+<div class="dropdownmenu_dolphin">
+
+<div id="dorpmenu_dolphin0" class="dorpmenu_dolphin">
+	<a href="temp5/index.php" class="dorpmenu_dolphin">网站首页</a>
+</div>
+<div id="subdorpmenu_dolphin0" class="subdorpmenu_dolphin">
+
+</div>
+
+<div id="dorpmenu_dolphin1" class="dorpmenu_dolphin">
+	<a href="temp5/page/html/company.php" class="dorpmenu_dolphin">公司简介</a>
+</div>
+<div id="subdorpmenu_dolphin1" class="subdorpmenu_dolphin">
+<ul>
+
+<li id="dropmenuli_dolphin402" ><a href="temp5/page/html/company.php" class="subdorpmenu_dolphin">公司简介</a></li>
+
+<li id="dropmenuli_dolphin403" ><a href="temp5/page/html/culture.php" class="subdorpmenu_dolphin">企业文化</a></li>
+
+<li id="dropmenuli_dolphin404" ><a href="temp5/page/html/honor.php" class="subdorpmenu_dolphin">企业荣誉</a></li>
+
+<li id="dropmenuli_dolphin405" ><a href="temp5/page/html/display.php" class="subdorpmenu_dolphin">厂房设备</a></li>
+
+<li id="dropmenuli_dolphin406" ><a href="temp5/page/html/sales.php" class="subdorpmenu_dolphin">营销网络</a></li>
+
+<li id="dropmenuli_dolphin407" ><a href="temp5/page/html/words.php" class="subdorpmenu_dolphin">总经理致辞</a></li>
+</ul>
+
+</div>
+
+<div id="dorpmenu_dolphin2" class="dorpmenu_dolphin">
+	<a href="temp5/page/html/honor.php" class="dorpmenu_dolphin">企业荣誉</a>
+</div>
+<div id="subdorpmenu_dolphin2" class="subdorpmenu_dolphin">
+
+</div>
+
+<div id="dorpmenu_dolphin3" class="dorpmenu_dolphin">
+	<a href="temp5/product/class/" class="dorpmenu_dolphin">产品中心</a>
+</div>
+<div id="subdorpmenu_dolphin3" class="subdorpmenu_dolphin">
+<ul>
+
+<li id="dropmenuli_dolphin412" ><a href="temp5/product/class/?1.html" class="subdorpmenu_dolphin">汽机车零件</a></li>
+
+<li id="dropmenuli_dolphin413" ><a href="temp5/product/class/?2.html" class="subdorpmenu_dolphin">园林工具零件</a></li>
+
+<li id="dropmenuli_dolphin414" ><a href="temp5/product/class/?3.html" class="subdorpmenu_dolphin">齿轮系列</a></li>
+
+<li id="dropmenuli_dolphin415" ><a href="temp5/product/class/?4.html" class="subdorpmenu_dolphin">交直流工具</a></li>
+
+<li id="dropmenuli_dolphin416" ><a href="temp5/product/class/?5.html" class="subdorpmenu_dolphin">结构零件</a></li>
+</ul>
+
+</div>
+
+<div id="dorpmenu_dolphin4" class="dorpmenu_dolphin">
+	<a href="temp5/news/class/" class="dorpmenu_dolphin">新闻动态</a>
+</div>
+<div id="subdorpmenu_dolphin4" class="subdorpmenu_dolphin">
+<ul>
+
+<li id="dropmenuli_dolphin417" ><a href="temp5/news/class/?86.html" class="subdorpmenu_dolphin">公司新闻</a></li>
+
+<li id="dropmenuli_dolphin418" ><a href="temp5/news/class/?95.html" class="subdorpmenu_dolphin">行业动态</a></li>
+</ul>
+
+</div>
+
+<div id="dorpmenu_dolphin5" class="dorpmenu_dolphin">
+	<a href="temp5/page/quality/goal.php" class="dorpmenu_dolphin">质量体系</a>
+</div>
+<div id="subdorpmenu_dolphin5" class="subdorpmenu_dolphin">
+<ul>
+
+<li id="dropmenuli_dolphin408" ><a href="temp5/page/quality/goal.php" class="subdorpmenu_dolphin">质量目标</a></li>
+
+<li id="dropmenuli_dolphin409" ><a href="temp5/page/quality/policy.php" class="subdorpmenu_dolphin">质量方针</a></li>
+</ul>
+
+</div>
+
+<div id="dorpmenu_dolphin6" class="dorpmenu_dolphin">
+	<a href="temp5/job/index.php" class="dorpmenu_dolphin">人才招聘</a>
+</div>
+<div id="subdorpmenu_dolphin6" class="subdorpmenu_dolphin">
+
+</div>
+
+<div id="dorpmenu_dolphin7" class="dorpmenu_dolphin">
+	<a href="temp5/page/contact/contact.php" class="dorpmenu_dolphin">联系我们</a>
+</div>
+<div id="subdorpmenu_dolphin7" class="subdorpmenu_dolphin">
+<ul>
+
+<li id="dropmenuli_dolphin410" ><a href="temp5/page/contact/contact.php" class="subdorpmenu_dolphin">联系方式</a></li>
+
+<li id="dropmenuli_dolphin411" ><a href="temp5/page/contact/feedback.php" class="subdorpmenu_dolphin">信息反馈</a></li>
+</ul>
+
+</div>
+ 
+</div>
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 全站搜索表单 -->
+
+<div id='pdv_23322' class='pdv_class'  title='全站搜索' style='width:298px;height:28px;top:46px;left:676px; z-index:13'>
+<div id='spdv_23322' class='pdv_top' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:12px;line-height:25px;font-weight:bold;color:">
+		全站搜索
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="temp5/-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+<link href="temp5/search/templates/css/searchform_simple.css" rel="stylesheet" type="text/css" />
+<script language="javascript" src="temp5/search/js/searchform.js"></script>
+
+<div class="globalsearchformzone">
+<form id="globalsearchform" method="get" action="search/index.php">
+   	<div class="globalsearchform">
+    	<input name="key" type="text" id="globalsearchform_key" value="" class="input" />
+  	</div>
+	<div class="globalsearchform1">
+  		<input name="imageField" id="button" type="image" src="temp5/search/templates/images/searchr.jpg" class="imgbutton" />
+  	</div>
+</form>
+</div>
+
+</div>
+</div>
+
+</div>
+</div>
+</div>
+
---
+++ trunk/b2b/www/company/temp5/product.php
@@ -0,0 +1,233 @@
+<?php include "header.php"; ?>
+<script>
+var PDV_PAGEID='568'; 
+var PDV_RP='temp5/'; 
+var PDV_COLTYPE='product'; 
+var PDV_PAGENAME='query'; 
+</script>
+
+<div id='content' style='width:1002px;height:465px;background:url(temp5/effect/source/bg/cbg.jpg);margin:0px auto'>
+
+
+<!-- 产品分类（列表） -->
+
+<div id='pdv_23485' class='pdv_class'  title='' style='width:210px;height:267px;top:13px;left:9px; z-index:6'>
+<div id='spdv_23485' class='pdv_content' style='overflow:visible;width:100%;'>
+<div class="pdv_border" style="border:0px;height:100%;padding:0;margin:0;background:url(temp5/base/border/781/images/lbg.jpg) repeat-y;">
+<div style="height:100%;background:url(temp5/base/border/781/images/rbg.jpg) right repeat-y;">
+<div style="height:48px;border:0px;padding:0;margin:0;background:url(temp5/base/border/781/images/title.jpg) 0px 0px no-repeat;">
+</div>
+<div style="margin:0px;padding:0px;">
+
+<link href="temp5/product/templates/css/productclass_dolphin.css" rel="stylesheet" type="text/css" />
+<div class="productclass_dolphin">
+
+
+<a href="temp5/product/class/?1.html" target="_self" class="productclass_dolphin">汽机车零件</a>
+
+
+<a href="temp5/product/class/?2.html" target="_self" class="productclass_dolphin">园林工具零件</a>
+
+
+<a href="temp5/product/class/?3.html" target="_self" class="productclass_dolphin">齿轮系列</a>
+
+
+<a href="temp5/product/class/?4.html" target="_self" class="productclass_dolphin">交直流工具</a>
+
+
+<a href="temp5/product/class/?5.html" target="_self" class="productclass_dolphin">结构零件</a>
+ 
+</div>
+
+</div>
+</div>
+</div>
+<div style="margin-top:-15px;height:15px;line-height:15px;background:url(temp5/base/border/781/images/title.jpg) 0px -252px no-repeat">&nbsp;</div>
+<div style="float:right;margin-top:-15px;width:10px;height:15px;line-height:15px;background:url(temp5/base/border/781/images/title.jpg) -200px -252px no-repeat">&nbsp;</div>
+
+</div>
+</div>
+
+<!-- 产品搜索表单 -->
+
+<div id='pdv_23483' class='pdv_class'  title='产品搜索' style='width:210px;height:175px;top:290px;left:9px; z-index:12'>
+<div id='spdv_23483' class='pdv_content' style='overflow:visible;width:100%;'>
+<div class="pdv_border" style="border:0px;height:100%;padding:0;margin:0;background:url(temp5/base/border/787/images/lbg.jpg) repeat-y;">
+<div style="height:100%;background:url(temp5/base/border/787/images/rbg.jpg) right repeat-y;">
+<div style="height:48px;border:0px;padding:0;margin:0;background:url(temp5/base/border/787/images/title.jpg) 0px 0px no-repeat;">
+</div>
+<div style="margin:0px;padding:0px;">
+<link href="temp5/product/templates/css/product_searchform_dolphin.css" rel="stylesheet" type="text/css" />
+<script language="javascript" src="temp5/product/js/selectbox.js"></script>
+<div class="productsearchformzone">
+<form id="productsearchform" method="get" action="temp5/product/class/index.php">
+  
+   <div class="productsearchform">
+    <select name="catid" id="catid" class="box" style="display:none; background:#ffffff; width:155px; height:19px;">
+      <option value="0" >请选择分类</option>
+      
+	  <option value='1'>汽机车零件</option><option value='2'>园林工具零件</option><option value='3'>齿轮系列</option><option value='4'>交直流工具</option><option value='5'>结构零件</option>
+    
+    </select> 
+	<script>
+	makeSelectBox('catid','temp5/');
+	</script>
+  </div>
+    <div class="productsearchform">
+    <input name="key" type="text" id="productsearchform_key" value="" style="width:153px;" class="inputtext">
+  </div>
+  <div class="productsearchform2">
+  <input name="imageField" id="button" type="image" src="temp5/product/templates/images/search5.gif" >
+  </div>
+</form>
+</div>
+
+</div>
+</div>
+</div>
+<div style="margin-top:-15px;height:15px;line-height:15px;background:url(temp5/base/border/787/images/title.jpg) 0px -252px no-repeat">&nbsp;</div>
+<div style="float:right;margin-top:-15px;width:10px;height:15px;line-height:15px;background:url(temp5/base/border/787/images/title.jpg) -200px -252px no-repeat">&nbsp;</div>
+
+</div>
+</div>
+
+<!-- 产品检索搜索 -->
+
+<div id='pdv_23494' class='pdv_class'  title='产品检索' style='width:708px;height:70px;top:80px;left:263px; z-index:13'>
+<div id='spdv_23494' class='pdv_content' style='overflow:visible;width:100%;'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:12px;line-height:25px;font-weight:bold;color:">
+		产品检索
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+
+<link href="temp5/product/templates/css/productquery_dolphin.css" rel="stylesheet" type="text/css" />
+
+<div class="productquery_dolphin">
+    <div class="productquery_dolphin_top">
+		<div class="fang" style="width:162px;height:122px">
+		<div class="picFit" style="width:162px;height:122px">
+		<a href="temp5/product/html/?3.html" target="_self" ><img src="temp5/product/pics/20100420/1271741726.jpg" style="width:162px;height:122px" border="0" /></a>
+		</div>
+		</div>
+	</div>
+	<div class="productquery_dolphin_bottom">
+		<a href="temp5/product/html/?3.html" target="_self" class="prodtitle"   >交直流工具</a>
+	</div>
+</div>
+
+
+<div class="productquery_dolphin">
+    <div class="productquery_dolphin_top">
+		<div class="fang" style="width:162px;height:122px">
+		<div class="picFit" style="width:162px;height:122px">
+		<a href="temp5/product/html/?4.html" target="_self" ><img src="temp5/product/pics/20100420/1271741746.jpg" style="width:162px;height:122px" border="0" /></a>
+		</div>
+		</div>
+	</div>
+	<div class="productquery_dolphin_bottom">
+		<a href="temp5/product/html/?4.html" target="_self" class="prodtitle"   >汽机车零件</a>
+	</div>
+</div>
+
+
+<div class="productquery_dolphin">
+    <div class="productquery_dolphin_top">
+		<div class="fang" style="width:162px;height:122px">
+		<div class="picFit" style="width:162px;height:122px">
+		<a href="temp5/product/html/?9.html" target="_self" ><img src="temp5/product/pics/20100715/1279160644.jpg" style="width:162px;height:122px" border="0" /></a>
+		</div>
+		</div>
+	</div>
+	<div class="productquery_dolphin_bottom">
+		<a href="temp5/product/html/?9.html" target="_self" class="prodtitle"   >园林工具零件</a>
+	</div>
+</div>
+
+
+<div class="productquery_dolphin">
+    <div class="productquery_dolphin_top">
+		<div class="fang" style="width:162px;height:122px">
+		<div class="picFit" style="width:162px;height:122px">
+		<a href="temp5/product/html/?7.html" target="_self" ><img src="temp5/product/pics/20100420/1271741835.jpg" style="width:162px;height:122px" border="0" /></a>
+		</div>
+		</div>
+	</div>
+	<div class="productquery_dolphin_bottom">
+		<a href="temp5/product/html/?7.html" target="_self" class="prodtitle"   >园林工具零件</a>
+	</div>
+</div>
+
+
+<div class="productquery_dolphin">
+    <div class="productquery_dolphin_top">
+		<div class="fang" style="width:162px;height:122px">
+		<div class="picFit" style="width:162px;height:122px">
+		<a href="temp5/product/html/?5.html" target="_self" ><img src="temp5/product/pics/20100420/1271741780.jpg" style="width:162px;height:122px" border="0" /></a>
+		</div>
+		</div>
+	</div>
+	<div class="productquery_dolphin_bottom">
+		<a href="temp5/product/html/?5.html" target="_self" class="prodtitle"   >汽机车零件</a>
+	</div>
+</div>
+
+
+<div class="productquery_dolphin">
+    <div class="productquery_dolphin_top">
+		<div class="fang" style="width:162px;height:122px">
+		<div class="picFit" style="width:162px;height:122px">
+		<a href="temp5/product/html/?6.html" target="_self" ><img src="temp5/product/pics/20100420/1271741811.jpg" style="width:162px;height:122px" border="0" /></a>
+		</div>
+		</div>
+	</div>
+	<div class="productquery_dolphin_bottom">
+		<a href="temp5/product/html/?6.html" target="_self" class="prodtitle"   >汽机车零件</a>
+	</div>
+</div>
+
+
+<script>
+$(function() {
+$().picFit("auto");
+});
+</script>
+<div id="showpages" >
+	  <div id="pagesinfo">共6条 每页20条 页次：1/1</div>
+	  <div id="pages"><ul><li class="pbutton"><a href=/1294375486/product/class/index.php?page=1&amp;catid=0&amp;myord=uptime&amp;myshownums=&amp;showtj=&amp;author=&amp;key=>首页</a></li><li class="pbutton">上一页</li><li class="pagesnow">1</li><li class="pbutton">下一页</li><li class="opt"><select onChange="window.location=this.options[this.selectedIndex].value"><option value="/1294375486/product/class/index.php?page=1&amp;catid=0&amp;myord=uptime&amp;myshownums=&amp;showtj=&amp;author=&amp;key=" selected>第1页</option></select></li><li class="pbutton"><a href=/1294375486/product/class/index.php?page=1&amp;catid=0&amp;myord=uptime&amp;myshownums=&amp;showtj=&amp;author=&amp;key=>尾页</a></li></ul></div>
+     </div>
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 当前位置提示条 -->
+
+<div id='pdv_23495' class='pdv_class'  title='产品检索' style='width:730px;height:36px;top:13px;left:250px; z-index:14'>
+<div id='spdv_23495' class='pdv_content' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="border:0px;height:100%;padding:0;margin:0;background:url(temp5/base/border/792/images/title.jpg) 0px 0px no-repeat">
+	<div style="float:left;font:14px/32px Simsun;color:#000;padding-left:47px;">产品检索</div>
+ 
+<link href="temp5/product/templates/css/nav.css" rel="stylesheet" type="text/css" />
+<div id="nav">
+您现在的位置：<a href="temp5/">机械零部件制造公司</a> 
+ 
+&gt; <a href="temp5/product/" >产品展示</a> 
+
+</div>
+<div class="img"></div>
+
+</div>
+
+</div>
+</div>
+</div>
+
+<?php include "footer.php"; ?>
