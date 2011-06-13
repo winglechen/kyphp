e2cfe4518d8b6a271984a6d10afa4495df0f2249
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-06-13 05:18:01 +0000

    add style 1

---
+++ trunk/b2b/www/company/temp1/footer.php
@@ -0,0 +1,96 @@
+<div id='bottom' style='width:1002px;height:121px;background:none transparent scroll repeat 0% 0%'>
+
+
+<!-- 图片/FLASH -->
+
+<div id='pdv_10801' class='pdv_class'  title='' style='width:1002px;height:42px;top:0px;left:0px; z-index:4'>
+<div id='spdv_10801' class='pdv_bottom' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:6px;line-height:25px;font-weight:bold;color:">
+		
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="temp1/-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+
+
+<img src="temp1/diy/pics/20100719/1279529832.jpg" border="0" width="100%" />
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 底部菜单（一级） -->
+
+<div id='pdv_10813' class='pdv_class'   style='width:1002px;height:31px;top:6px;left:0px; z-index:5'>
+<div id='spdv_10813' class='pdv_bottom' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:6px;line-height:25px;font-weight:bold;color:">
+		脚注栏目
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="temp1/-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+
+<link href="temp1/menu/templates/images/bottommenu_1/A.css" rel="stylesheet" type="text/css" />
+<div id="bottommenu">|
+
+<a href="temp1/page/html/company.php" target="_self">关于我们</a>|
+
+<a href="temp1/page/contact/contact.php" target="_self">联系方式</a>|
+
+<a href="temp1/page/contact/feedback.php" target="_self">信息反馈</a>|
+
+<a href="temp1/page/join/application.php" target="_self">加盟申请</a>|
+
+<a href="temp1/page/join/display.php" target="_self">门店展示</a>|
+
+<a href="temp1/advs/link/" target="_self">友情链接</a>|
+ 
+</div>
+
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 底部信息编辑区 -->
+
+<div id='pdv_10807' class='pdv_class'  title='脚注信息' style='width:1002px;height:60px;top:59px;left:0px; z-index:11'>
+<div id='spdv_10807' class='pdv_bottom' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:6px;line-height:25px;font-weight:bold;color:">
+		脚注信息
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="temp1/-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+<div style="width:100%;text-align:center;font:12px/20px Arial, Helvetica, sans-serif">
+<div style="TEXT-ALIGN: center; LINE-HEIGHT: 24px; MARGIN: 0px; FONT-FAMILY: Verdana, Arial, Helvetica, sans-serif; COLOR: #cbcbcb">版权所有 Copyright(C)2009-2010 杭州某某整体橱柜公司</div>
+</div>
+
+</div>
+</div>
+
+</div>
+</div>
+</div>
+</div><div id='bodyex'>
+
+</div>
+
+</body>
+</html>
---
+++ trunk/b2b/www/company/temp1/header.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=7" />
+<title>-整体橱柜公司</title>
+<meta content="" name=keywords />
+<meta content="" name=description />
+<link href="temp1/base/templates/css/common.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="temp1/base/js/base.js"></script>
+<script type="text/javascript" src="temp1/base/js/common.js"></script>
+<script type="text/javascript" src="temp1/base/js/form.js"></script>
+<script type="text/javascript" src="temp1/base/js/blockui.js"></script>
+<!-reload-!>
+</head>
+<body style='background:transparent url(temp1/effect/source/bg/bg.gif) repeat scroll 0% 0%'>
+<div id='contain' style='width:1002px;background:url(temp1/effect/source/bg/1262661247.gif) repeat-y left top;margin:0px auto;padding:0px'>
+
+<div id='top' style='width:1002px;height:390px;background:url(temp1/effect/source/bg/1262918621.gif)'>
+
+
+<!-- 自定义透明FLASH -->
+
+<div id='pdv_14709' class='pdv_class'  title='' style='width:1002px;height:280px;top:103px;left:0px; z-index:1'>
+<div id='spdv_14709' class='pdv_top' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:6px;line-height:25px;font-weight:bold;color:">
+		
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="temp1/-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+
+
+<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" width="1002" height="280">
+ <param name="movie" value="diy/pics/20101217/1292564238.swf" />
+ <param name="quality" value="high" />
+ <param name="wmode" value="transparent" />
+ <embed src="temp1/diy/pics/20101217/1292564238.swf" wmode="transparent" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="1002" height="280"></embed>
+</object>
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 图片/FLASH -->
+
+<div id='pdv_12532' class='pdv_class'  title='' style='width:1002px;height:103px;top:0px;left:0px; z-index:2'>
+<div id='spdv_12532' class='pdv_top' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:6px;line-height:25px;font-weight:bold;color:">
+		
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="temp1/-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+
+
+<img src="temp1/diy/pics/20100719/1279514809.jpg" border="0" width="100%" />
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 一级导航菜单 -->
+
+<div id='pdv_10994' class='pdv_class'   style='width:730px;height:20px;top:74px;left:272px; z-index:3'>
+<div id='spdv_10994' class='pdv_top' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:6px;line-height:25px;font-weight:bold;color:">
+		导航菜单
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="temp1/-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+
+<link href="temp1/menu/templates/images/mainmenu_dolphin/A.css" rel="stylesheet" type="text/css" />
+<div id="menumain_dolphin">
+<div class="mainmenuiner">
+
+<a href="temp1/index.php" target="_self" class="menumain_dolphin">网站首页</a>
+
+<a href="temp1/page/html/company.php" target="_self" class="menumain_dolphin">公司简介</a>
+
+<a href="temp1/news/class/" target="_self" class="menumain_dolphin">新闻动态</a>
+
+<a href="temp1/product/class" target="_self" class="menumain_dolphin">产品系列</a>
+
+<a href="temp1/page/join/notice.php" target="_self" class="menumain_dolphin">品牌加盟</a>
+
+<a href="temp1/page/job/job.php" target="_self" class="menumain_dolphin">人才招聘</a>
+
+<a href="temp1/page/service/service.php" target="_self" class="menumain_dolphin">客户服务</a>
+
+<a href="temp1/page/contact/contact.php" target="_self" class="menumain_dolphin">联系我们</a>
+
+</div>
+</div>
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 网站标志 -->
+
+<div id='pdv_10806' class='pdv_class'  title='' style='width:253px;height:85px;top:0px;left:14px; z-index:6'>
+<div id='spdv_10806' class='pdv_top' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:6px;line-height:25px;font-weight:bold;color:">
+		
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="temp1/-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+
+
+<img src="temp1/advs/pics/20100719/1279516084.jpg" border="0" />
+
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 图片/FLASH -->
+
+<div id='pdv_13604' class='pdv_class'  title='' style='width:1002px;height:5px;top:383px;left:0px; z-index:8'>
+<div id='spdv_13604' class='pdv_top' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:6px;line-height:25px;font-weight:bold;color:">
+		
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="temp1/-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+
+
+<img src="temp1/diy/pics/20100928/1285643581.jpg" border="0" width="100%" />
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 全站搜索表单 -->
+
+<div id='pdv_14089' class='pdv_class'  title='全站搜索' style='width:296px;height:30px;top:20px;left:684px; z-index:9'>
+<div id='spdv_14089' class='pdv_top' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:6px;line-height:25px;font-weight:bold;color:">
+		全站搜索
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="temp1/-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+<link href="temp1/search/templates/css/searchform_simple.css" rel="stylesheet" type="text/css" />
+<script language="javascript" src="temp1/search/js/searchform.js"></script>
+
+<div class="globalsearchformzone">
+<form id="globalsearchform" method="get" action="search/index.php">
+   	<div class="globalsearchform">
+    	<input name="key" type="text" id="globalsearchform_key" value="" class="input" />
+  	</div>
+	<div class="globalsearchform1">
+  		<input name="imageField" id="button" type="image" src="temp1/search/templates/images/searchr.jpg" class="imgbutton" />
+  	</div>
+</form>
+</div>
+
+</div>
+</div>
+
+</div>
+</div>
+</div>
