849b1baae882c601253d404a5fb51eacad97ee7a
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-06-13 05:49:18 +0000

    add template4

---
+++ b2b/www/company/temp4/news.php
@@ -0,0 +1,224 @@
+<?php include "header.php"; ?>
+<script>
+var PDV_PAGEID='2'; 
+var PDV_RP='temp4/'; 
+var PDV_COLTYPE='news'; 
+var PDV_PAGENAME='query'; 
+</script>
+
+<div id='content' style='width:1002px;height:530px;background:url(temp4/effect/source/bg/cbg.jpg) repeat-y;margin:0px auto'>
+
+
+<!-- 图片/FLASH -->
+
+<div id='pdv_21920' class='pdv_class'  title='' style='width:990px;height:11px;top:0px;left:6px; z-index:1'>
+<div id='spdv_21920' class='pdv_content' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:12px;line-height:25px;font-weight:bold;color:">
+		
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+
+
+<a href="temp4/page/contact/feedback.php"><img src="temp4/diy/pics/20100903/1283493085.jpg" border="0" width="100%" /></a>
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 产品分类（列表） -->
+
+<div id='pdv_21926' class='pdv_class'  title='产品分类' style='width:171px;height:267px;top:152px;left:35px; z-index:11'>
+<div id='spdv_21926' class='pdv_content' style='overflow:visible;width:100%;'>
+<div class="pdv_border" style="border:0px;height:100%;padding:0;margin:0;">
+<div style="height:38px;border:0px;padding:0;margin:0;background:url(temp4/base/border/787/images/title.jpg) 0px 0px no-repeat;">
+</div>
+<div style="margin:0px;padding:0px;">
+
+<link href="temp4/product/templates/css/productclass_dolphin.css" rel="stylesheet" type="text/css" />
+<div class="productclass_dolphin">
+
+
+<a href="temp4/product/class/?1.html" target="_self" class="productclass_dolphin">莱卡生物显微镜</a>
+
+
+<a href="temp4/product/class/?2.html" target="_self" class="productclass_dolphin">莱卡手术显微镜</a>
+
+
+<a href="temp4/product/class/?3.html" target="_self" class="productclass_dolphin">奥林巴斯医疗产品</a>
+
+
+<a href="temp4/product/class/?4.html" target="_self" class="productclass_dolphin">内镜清洗机</a>
+
+
+<a href="temp4/product/class/?5.html" target="_self" class="productclass_dolphin">牙科产品</a>
+
+
+<a href="temp4/product/class/?6.html" target="_self" class="productclass_dolphin">GE心电产品系列</a>
+
+
+<a href="temp4/product/class/?10.html" target="_self" class="productclass_dolphin">血液透析产品</a>
+
+
+<a href="temp4/product/class/?11.html" target="_self" class="productclass_dolphin">其它设备</a>
+ 
+</div>
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 当前位置提示条 -->
+
+<div id='pdv_21932' class='pdv_class'  title='当前位置' style='width:764px;height:40px;top:1px;left:232px; z-index:12'>
+<div id='spdv_21932' class='pdv_content' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="border:0px;height:100%;padding:0;margin:0;background:url(temp4/base/border/792/images/title.jpg) 0px 0px no-repeat">
+ 
+<link href="temp4/news/templates/css/nav.css" rel="stylesheet" type="text/css" />
+<div id="nav">
+您现在的位置：<a href="temp4/">医疗器械公司</a> 
+ 
+&gt; <a href="temp4/news/" >新闻中心</a> 
+
+</div>
+<div class="img"></div>
+
+</div>
+
+</div>
+</div>
+
+<!-- 产品搜索表单 -->
+
+<div id='pdv_21927' class='pdv_class'  title='产品搜索' style='width:171px;height:120px;top:27px;left:35px; z-index:13'>
+<div id='spdv_21927' class='pdv_content' style='overflow:hidden;width:100%;height:100%'>
+<div class="pdv_border" style="border:0px;height:100%;padding:0;margin:0;">
+<div style="height:38px;border:0px;padding:0;margin:0;background:url(temp4/base/border/784/images/title.jpg) 0px 0px no-repeat;">
+</div>
+<div style="margin:0px;padding:0px;">
+<link href="temp4/product/templates/css/product_searchform_dolphin.css" rel="stylesheet" type="text/css" />
+<script language="javascript" src="temp4/product/js/selectbox.js"></script>
+<div class="productsearchformzone">
+<form id="productsearchform" method="get" action="temp4/product/class/index.php">
+  
+   <div class="productsearchform">
+    <select name="catid" id="catid" class="box" style="display:none; background:#ffffff; width:130px; height:19px;">
+      <option value="0" >请选择分类</option>
+      
+	  <option value='1'>莱卡生物显微镜</option><option value='2'>莱卡手术显微镜</option><option value='3'>奥林巴斯医疗产品</option><option value='4'>内镜清洗机</option><option value='5'>牙科产品</option><option value='6'>GE心电产品系列</option><option value='10'>血液透析产品</option><option value='11'>其它设备</option>
+    
+    </select> 
+	<script>
+	makeSelectBox('catid','temp4/');
+	</script>
+  </div>
+    <div class="productsearchformleft">
+    <input name="key" type="text" id="productsearchform_key" value="" style="width:80px;" class="inputproduct">
+  </div>
+  <div class="productsearchform2">
+  <input name="imageField" id="button" type="image" src="temp4/product/templates/images/searchr.jpg" >
+  </div>
+</form>
+</div>
+
+</div>
+</div>
+
+</div>
+</div>
+
+<!-- 文章翻页检索 -->
+
+<div id='pdv_21931' class='pdv_class'  title='文章检索' style='width:688px;height:450px;top:78px;left:270px; z-index:14'>
+<div id='spdv_21931' class='pdv_content' style='overflow:visible;width:100%;'>
+<div class="pdv_border" style="margin:0;padding:0;height:100%;border:0px  solid;background:;">
+	<div style="height:25px;margin:1px;display:none;background:;">
+		<div style="float:left;margin-left:12px;line-height:25px;font-weight:bold;color:">
+		文章检索
+		</div>
+		<div style="float:right;margin-right:10px;display:none">
+		<a href="-1" style="line-height:25px;color:">更多</a>
+		</div>
+	</div>
+<div style="padding:0px">
+
+<link href="temp4/news/templates/css/newsquery.css" rel="stylesheet" type="text/css" />
+<div id="newsquery">
+<ul id="queryul">
+ 
+<li class="title">
+	<div class="title"><a href="temp4/news/html/?423.html" target="_self"   >参加华南国际口腔展概况</a></div>
+	<div class="time">2010-05-13</div>
+</li>
+ 
+<li class="title">
+	<div class="title"><a href="temp4/news/html/?427.html" target="_self"   >夏季常见病防治小知识</a></div>
+	<div class="time">2010-06-21</div>
+</li>
+ 
+<li class="title">
+	<div class="title"><a href="temp4/news/html/?426.html" target="_self"   >最适合中国人的防流感食物</a></div>
+	<div class="time">2010-06-21</div>
+</li>
+ 
+<li class="title">
+	<div class="title"><a href="temp4/news/html/?425.html" target="_self"   >夏季常见病防治小知识</a></div>
+	<div class="time">2010-06-21</div>
+</li>
+ 
+<li class="title">
+	<div class="title"><a href="temp4/news/html/?424.html" target="_self"   >清晨喝水保健功效最好</a></div>
+	<div class="time">2010-06-21</div>
+</li>
+ 
+<li class="title">
+	<div class="title"><a href="temp4/news/html/?419.html" target="_self"   >我国药品注册管理出现新变化我国药品注册管理出现新变化</a></div>
+	<div class="time">2010-04-20</div>
+</li>
+ 
+<li class="title">
+	<div class="title"><a href="temp4/news/html/?420.html" target="_self"   >政策利空药企发改委划区调研降药价政策利空药企发改委划区调研降药价</a></div>
+	<div class="time">2010-05-13</div>
+</li>
+ 
+<li class="title">
+	<div class="title"><a href="temp4/news/html/?421.html" target="_self"   >我国药品注册管理出现新变化我国药品注册管理出现新变化</a></div>
+	<div class="time">2010-05-13</div>
+</li>
+ 
+<li class="title">
+	<div class="title"><a href="temp4/news/html/?422.html" target="_self"   >政策利空药企 发改委划区调研降药价</a></div>
+	<div class="time">2010-05-13</div>
+</li>
+ 
+<li class="title">
+	<div class="title"><a href="temp4/news/html/?321.html" target="_self"   >30家知名媒体聚焦药业自主创新家知名媒体聚焦药业自主创新</a></div>
+	<div class="time">2009-12-10</div>
+</li>
+
+</ul>
+</div>
+
+<div id="showpages">
+	  <div id="pagesinfo">共14条 每页10条 页次：1/2</div>
+	  <div id="pages"><ul><li class="pbutton"><a href=/1294374538/news/class/index.php?page=1&amp;catid=0&amp;myord=uptime&amp;myshownums=&amp;showtj=&amp;showdate=&amp;author=&amp;key=>首页</a></li><li class="pbutton">上一页</li><li class="pagesnow">1</li><li><a href=/1294374538/news/class/index.php?page=2&amp;catid=0&amp;myord=uptime&amp;myshownums=&amp;showtj=&amp;showdate=&amp;author=&amp;key=>2</a></li><li class="pbutton"><a  href=/1294374538/news/class/index.php?page=2&amp;catid=0&amp;myord=uptime&amp;myshownums=&amp;showtj=&amp;showdate=&amp;author=&amp;key= >下一页</a></li><li class="opt"><select onChange="window.location=this.options[this.selectedIndex].value"><option value="/1294374538/news/class/index.php?page=1&amp;catid=0&amp;myord=uptime&amp;myshownums=&amp;showtj=&amp;showdate=&amp;author=&amp;key=" selected>第1页</option><option value="/1294374538/news/class/index.php?page=2&amp;catid=0&amp;myord=uptime&amp;myshownums=&amp;showtj=&amp;showdate=&amp;author=&amp;key=">第2页</option></select></li><li class="pbutton"><a href=/1294374538/news/class/index.php?page=2&amp;catid=0&amp;myord=uptime&amp;myshownums=&amp;showtj=&amp;showdate=&amp;author=&amp;key=>尾页</a></li></ul></div>
+</div>
+
+
+</div>
+</div>
+
+</div>
+</div>
+</div>
+
+<?php echo 'footer.php'; ?>
