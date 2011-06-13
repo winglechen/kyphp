63f0cffa147165a4e13f425db42dbcc36fe7a907
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-06-13 09:25:44 +0000

    remove the javascripts of main

--- b2b/cubex/page/www/member/main.php
+++ b2b/cubex/page/www/member/main.php
@@ -56,13 +56,13 @@
       <td>
       <ul>
       <li>
-      <a href="index.php?p=www/member/company_main" style="color:#2b71b6" onmouseout="javascript:toExit('',this);" onmouseover="javascript:toExit('完整填写的公司信息能够更好的彰显企业实力赢得客户信任。', this);">填写公司信息</a>
+      <a href="index.php?p=www/member/company_main" style="color:#2b71b6" >填写公司信息</a>
       </li>
        <li>
-       <a href="index.php?p=www/member/company_contact" onmouseout="javascript:toExit('',this);" onmouseover="javascript:toExit('发布证书荣誉，彰显企业实力赢得客户信任。', this);">填写联系方式</a>
+       <a href="index.php?p=www/member/company_contact" >填写联系方式</a>
        </li>     
       <li>
-      <a href="index.php?p=www/member/company_account" onmouseout="javascript:toExit('',this);" onmouseover="javascript:toExit('填写即时通讯帐号信息，客户可以直接在网站上和您直接在线沟通。', this);">修改登录密码</a>
+      <a href="index.php?p=www/member/company_account" >修改登录密码</a>
        </li>
       </ul>
       </td>
@@ -70,12 +70,12 @@
       <td>
       <ul>
         <li>
-        <a href="index.php?p=www/member/product_list" style="color:#ec6500" onmouseout="javascript:toExit('',this);" onmouseover="javascript:toExit('发布供应信息，让客户快速找到您', this);">发布供应</a></li>
+        <a href="index.php?p=www/member/product_list" style="color:#ec6500" >发布供应</a></li>
         
-        <li><a href="index.php?p=www/member/product_add" style="color:#ec6500" onmouseout="javascript:toExit('',this);" onmouseover="javascript:toExit('发布供应信息，让客户快速找到您', this);">查看供应</a></li>
+        <li><a href="index.php?p=www/member/product_add" style="color:#ec6500" >查看供应</a></li>
  
         <li>
-        <a href="index.php?p=www/member/qigou_add" onmouseout="javascript:toExit('',this);" onmouseover="javascript:toExit('发布求购信息，让供应商快速找到您。', this);">发布求购</a></li>
+        <a href="index.php?p=www/member/qigou_add" >发布求购</a></li>
                
       </ul>
       </td>
@@ -83,22 +83,22 @@
       <td>
       <ul>
         <li>
-        <a href="index.php?p=www/member/supply_add" onmouseout="javascript:toExit('',this);" onmouseover="javascript:toExit('产品系列帮您对自己的产品进行归类，您可根据自己的需要和喜好将所有的产品进行分类。&lt;br&gt;同时，您的产品在您的企业网站中也会依据创建的产品系列分门别类地显示。', this);">发布库存信息</a></li>
+        <a href="index.php?p=www/member/supply_add" >发布库存信息</a></li>
        
         <li>
-        <a href="index.php?p=www/member/supply_list" onmouseout="javascript:toExit('',this);" onmouseover="javascript:toExit('所谓橱窗：是指把贵司的拳头产品放置在您商铺上一个比较显眼的位置，吸引买家的注意力。&lt;br&gt;您可以选择最多16条供应信息布置在橱窗中。', this);">查看库存信息</a></li>
+        <a href="index.php?p=www/member/supply_list" >查看库存信息</a></li>
  
         <li>
-        <a href="index.php?p=www/member/qigou_list" onmouseout="javascript:toExit('',this);" onmouseover="javascript:toExit('发布求购信息，让供应商快速找到您。', this);">查看求购信息</a></li>
+        <a href="index.php?p=www/member/qigou_list" >查看求购信息</a></li>
      
       </ul>
       </td>
       <td>&nbsp;</td>
        <td>
       <ul> 
-        <li><a href="index.php?p=www/member/website_style" onmouseout="javascript:toExit('',this);" onmouseover="javascript:toExit('该功能仅向名企会员开放；名企会员可上传长达10分钟的视频文件，以宣传企业形象，吸引客户。', this);">网站模板选择</a></li>
-        <li><a  href="index.php?p=www/member/website_setting_add" onmouseout="javascript:toExit('',this);" onmouseover="javascript:toExit('该功能仅向名企会员和信用会员开放,使用信用标注后，当您的买家、合作伙伴、供货商及员工点击您网站上的中国信用图标时，即可直接到达您公司在中国供应商上的信用档案页面，从而充分了解贵公司的企业信用状况，进而直接提升企业品牌形象。', this);">网站logo设置</a></li>
-        <li><a href="index.php?p=www/member/website_new_list" onmouseout="javascript:toExit('',this);" onmouseover="javascript:toExit('1.商铺管理员是指您公司维护发布在“中国供应商”上信息的人员。&lt;br&gt;2.商铺管理员的信息不显示在您的商铺中。&lt;br&gt;3.商铺管理员的信息用于“中国供应商”为您提供服务联系之用。', this);">公司新闻管理</a></li>
+        <li><a href="index.php?p=www/member/website_style" >网站模板选择</a></li>
+        <li><a  href="index.php?p=www/member/website_setting_add" >网站logo设置</a></li>
+        <li><a href="index.php?p=www/member/website_new_list" >公司新闻管理</a></li>
       </ul>
       </td> 
     </tr>
