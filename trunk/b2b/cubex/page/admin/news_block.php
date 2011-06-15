4413ef509b92265b680b3d287184e734e15501dc
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-06-15 09:55:39 +0000

    add news_block.php

---
+++ trunk/b2b/cubex/page/admin/news_block.php
@@ -0,0 +1,102 @@
+<?php
+use Ky\Model\Pages;
+use Ky\Core\Core\Db;
+
+
+$data = array();
+$sql = "" ;
+
+if(!empty($_POST)){
+    $_SESSION['form']    = $_POST;
+    $_SESSION['formurl'] = $_SERVER['REQUEST_URI']; 
+}elseif(isset($_SESSION['form']) && !empty($_SESSION['form'])){
+    if(isset($_SESSION['formurl']) && $_SERVER['REQUEST_URI'] == $_SESSION['formurl']){
+        $_POST = $_SESSION['form']; 
+    }else{
+        $_SESSION['form']    = array();
+    }
+}
+
+
+if(!empty($_POST)){
+    if(isset($_POST['name'])){
+        $sql .= " and name like '%{$_POST['name']}%' ";
+    }
+}
+
+$sql_num = "select count(1) as num from news where 1 " . $sql;
+$tmp = Db::getRow($sql_num);
+$current_page   = isset($_POST['pageNum']) ? $_POST['pageNum'] : 1;
+$numPerPage     = 10;
+$nums           = $tmp['num'];
+$pages          = ceil($nums/$numPerPage);
+
+$sql_data = " select id,title from news where 1 " . $sql . " limit " . ($current_page - 1) * $numPerPage . "," . $numPerPage;
+$data = Db::getRows($sql_data);
+?>
+<div class="page">
+    <div class="pageHeader">
+        <form onsubmit="return navTabSearch(this);" id="pagerForm" action="index.php?p=admin/news_list" method="post">
+        <input type="hidden" name="pageNum" value="1">
+            <div class="searchBar">
+                <table class="searchContent">
+                    <tr>
+                        <td>
+                            新闻标题：<input type="text" name="title" />
+                        </td>
+                        <td>
+                            <div class="buttonActive"><div class="buttonContent"><button type="submit">检索</button></div></div>
+                        </td>
+                    </tr>
+                </table>
+                <!--div class="subBar">
+                    <ul>
+                        <li><div class="buttonActive"><div class="buttonContent"><button type="submit">检索</button></div></div></li>
+                    </ul>
+                </div-->
+            </div>
+        </form>
+    </div>
+    <div class="pageContent">
+        <div class="panelBar">
+            <ul class="toolBar">
+                <li>
+                    <div class="buttonActive"><div class="buttonContent"><button type="submit"> 选 择 </button></div></div>
+                </li>
+                <li class="line">line</li>
+            </ul>
+        </div>
+
+        <table class="list" width="98%" layoutH="116">
+            <thead>
+                <tr>
+                    <th width="5%" style="text-align:center" ><input type="checkbox" class="checkboxCtrl" group="rowid[]" ></th>
+                    <th width="5%" style="text-align:center" >编号</th>
+                    <th width="65%">标题</th>
+                    <th width="20$" style="text-align:center" >操作</th>
+                </tr>
+            </thead>
+            <tbody>
+
+<?php
+foreach($data as $new){
+    echo '<tr>';
+    echo '<td style="text-align:center" ><input type="checkbox" name="rowid[]" value="'.$new['id'].'" /></td>';
+    echo '<td style="text-align:center" >'.$new['id'].'</td>';
+    echo '<td>'.$new['title'].'</td>';
+    echo '<td style="text-align:center" ><a href="index.php?p=admin/news_add&id='.$new['id'].'" target="navTab" >编辑</a> | <a   href="index.php?p=admin/news_del&id='.$new['id'].'" target="navTab" onclick="return confirm(\'您确定要删除这条信息吗？\');" >删除</a></td>';
+    echo '</tr>';
+}
+?>
+            </tbody>
+        </table>
+
+        <div class="panelBar">
+            <div class="pages">
+            <span>共<?php echo $nums; ?>条</span>
+            </div>
+
+            <div class="pagination" targetType="navTab" totalCount="<?php echo $nums; ?>" numPerPage="<?php echo $numPerPage; ?>" pageNumShown="3" currentPage="<?php echo $current_page; ?>"></div>
+        </div>
+    </div>
+</div>
