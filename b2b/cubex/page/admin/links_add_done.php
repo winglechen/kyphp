b317f415c2c03ab44ec6a3b1e802a943fa5eb664
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-06-16 11:02:49 +0000

    add links and ad pages

---
+++ b2b/cubex/page/admin/links_add_done.php
@@ -0,0 +1,30 @@
+<?php
+use Ky\Core\Formater\Output;
+use Ky\Model\Links;
+use Ky\Core\Core\Path;
+use Ky\Core\Uid\Uid;
+
+
+if(!empty($_POST)){
+    
+    if($_FILES['pic']['name']){
+        $uid      = Uid::num();
+        $ext      = Path::getFileExt($_FILES['pic']['name']);
+        $fileName = IMG_PATH . $uid . '.' . $ext;
+        move_uploaded_file($_FILES['pic']['tmp_name'],$fileName);
+        $_POST['pic'] = IMG_HOST . $uid . '.' . $ext ;
+    }
+
+    if(0 == $_POST['id']){
+        Links::add($_POST);
+    }else{
+        Links::update($_POST);
+    }
+    
+    Output::iframeCallback(200,'添加成功','index.php?p=admin/links_list');
+    //header('Location:index.php?p=admin/intro_list&cid='.$cid);
+}else{
+    Output::iframeCallback(300,'添加失败');
+}
+
+
--- b2b/cubex/page/admin/links_del.php
+++ b2b/cubex/page/admin/links_del.php
@@ -1,11 +1,11 @@
 <?php
-use Ky\Model\News;
+use Ky\Model\Links;
 use Ky\Core\Formater\Output;
 
 $id = $_GET['id'];
 
-News::delete($id);
+Links::delete($id);
 
 //Output::iframeCallback(200,'添加成功','index.php?p=admin/intro_list&cid='.$cid);
-header('Location:index.php?p=admin/news_list);
+header('Location:index.php?p=admin/link_list);
 
