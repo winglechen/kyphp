1348394d4a52835ad8a88b2ae377d70aec8e4df8
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-07-05 08:27:11 +0000

    add member_category.sql

---
+++ trunk/b2b/sql/member_category.sql
@@ -0,0 +1,12 @@
+use b2b;
+set names utf8;
+
+drop table if exists member_category;
+create table member_category(
+    pid mediumint(8) unsigned not null default 0 comment '公司id',
+    id mediumint(8) unsigned not null  ,
+    name varchar(50) not null default '',
+    unique key(pid,id)
+)engine=myisam default charset=utf8 collate=utf8_general_ci;
+
+
