94b74d9aca041e52cdf18a69714628b0421959b2
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-07-08 05:35:43 +0000

    change logo

--- b2b/cubex/page/www/header_logo.php
+++ b2b/cubex/page/www/header_logo.php
@@ -2,7 +2,7 @@
 <!--logo区 begin-->
 <div class="logo">
 	<a href="#" target="_blank">
-	    <img src="./res/logo_index.png"  alt="" style="margin:3px 0 0 0;float:left;">
+	    <img src="./logo/logo_index.jpg"  alt="" style="margin:3px 0 0 0;float:left;">
 	</a>
 <?php
     include "search_box.php";
