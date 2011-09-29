9b18a7f97707df725fe99a66908c78cdfb88ce93
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-09-29 13:25:33 +0000

    nav change

--- trunk/b2b/cubex/page/www/navigation.php
+++ trunk/b2b/cubex/page/www/navigation.php
@@ -3,8 +3,8 @@
     <li><a href="index.php">找产品</a></li>
     <li><a href="index.php?p=www/qiugou">找求购</a></li>    
     <li><a href="index.php?p=www/company">找公司</a></li>
-    <li><a href="index.php?p=www/supply">找库存</a></li>
     <li><a href="index.php?p=www/new">商业资讯</a></li>
-    <li><a href="http://bbs.ztb2b.com">经理人社区</a></li>
+    <li><a href="http://www.haomingchina.com/" target="_blank">商城</a></li>
+    <li><a href="http://bbs.ztb2b.com" target="_blank">商交社区</a></li>
     <li class="nav_end"></li>                  
 </ul>
