ee42bd3dd95bee43c656fc765d90612ff24cf9b5
winglechen <winglechen@394b337e-edb2-f9a0-22a1-1b636049baf7>
2011-12-13 06:17:15 +0000

    seo change

--- trunk/b2b/cubex/page/www/header.php
+++ trunk/b2b/cubex/page/www/header.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-<title>商交网--中小企业发展平台</title>
-<meta name="description" content="">
-<meta name="keywords" content="">
+<title>商交网,商交,中国供应商,B2B,中国求购网,中国供应网,中小企业发展示平台</title>
+<meta name="description" content="中国供应商 , 商交网, 电子商务 ,B2B, 网上贸易 , 供应信息 , B2B 搜索引擎 , 电子商务 , 买卖信息 , 贸易机会 , 商业信息 , 供求信息 , 求购信息，免费建站">
+<meta name="keywords" content="商交网 , 中国供应商 , 电子商务 ,B2B, 网上贸易 , 供应信息 , B2B 搜索引擎 , 电子商务 , 买卖信息 , 贸易机会">
 <link href="./res/topfot555.css" rel="stylesheet" type="text/css">
 <link href="./res/index555.css" rel="stylesheet" type="text/css">
 
